079194a46ba1a98736ed56a09ebd6a5738855265
Alex Dolski <alexd@n-ary.net>
2014-03-06 16:33:13 -0600

    Squelch a warning message

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module Psap
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.i18n.enforce_available_locales = true
   end
 end
