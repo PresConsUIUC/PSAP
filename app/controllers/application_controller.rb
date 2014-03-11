8749a9e8a0610b407b594a1e80e4563fe7bf5167
Alex Dolski <alexd@n-ary.net>
2014-03-11 16:51:01 -0500

    Improve title menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :init
+
   layout 'application'
 
   include SessionsHelper
+
+  private
+  def init
+    # Array of all repositories associated with the current user, to appear in
+    # the title menu.
+    @user_institution_repositories = current_user.institution.repositories.order(:name)
+  end
+
 end
