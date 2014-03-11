673235fd7ac9d6de553fc2cad36ac4936d9ded37
Alex Dolski <alexd@n-ary.net>
2014-03-11 16:27:04 -0500

    Set session duration to 1 hour

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-Psap::Application.config.session_store :cookie_store, key: '_psap_session'
+# 1-hour sessions are specified in the PSAP Software Requirements Specification
+Psap::Application.config.session_store :cookie_store, key: '_psap_session',
+                                       expire_after: 1.hour
