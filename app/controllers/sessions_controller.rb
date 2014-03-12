e39741823f9c1e5c4ac5518c95f735612ec8a03f
Alex Dolski <alexd@n-ary.net>
2014-03-12 08:41:00 -0500

    Users log in with username instead of email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by(username: params[:session][:username].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
       redirect_back_or user
     else
       sleep 2 # slow down brute-force attacks
-      flash.now[:error] = 'Invalid email/password combination.'
+      flash.now[:error] = 'Invalid username/password combination.'
       render 'new'
     end
   end
