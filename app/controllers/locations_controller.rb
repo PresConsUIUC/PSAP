dd98942dbc18d766f16562223caeb5fdc3226fb6
Alex Dolski <alexd@n-ary.net>
2014-03-13 14:44:46 -0500

    Add locations resource

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,55 @@
+class LocationsController < ApplicationController
+
+  before_action :signed_in_user
+
+  def create
+    @location = Location.new(location_params)
+    if @location.save
+      flash[:success] = 'Location created.'
+      redirect_to @location
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    location = Location.find(params[:id])
+    name = location.name
+    location.destroy
+    flash[:success] = "#{name} deleted."
+    redirect_to locations_url
+  end
+
+  def edit
+    @location = Location.find(params[:id])
+  end
+
+  def index
+    @locations = Location.paginate(page: params[:page], per_page: 30)
+  end
+
+  def new
+    @location = Location.new
+  end
+
+  def show
+    @location = Location.find(params[:id])
+  end
+
+  def update
+    @location = Location.find(params[:id])
+    if @location.update_attributes(location_params)
+      flash[:success] = 'Location updated.'
+      redirect_to @location
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:location).permit(:name, :repository)
+  end
+
+end
