3d2d6055a021ab2b7c9428c475c7175364326010
Alex Dolski <alexd@n-ary.net>
2014-03-17 16:35:10 -0500

    Show assessments on institution's show page

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -2,6 +2,11 @@ class Institution < ActiveRecord::Base
   has_many :users, inverse_of: :institution, dependent: :restrict
   has_many :repositories, inverse_of: :institution, dependent: :destroy
 
+  has_many :locations, through: :repositories
+  has_many :resources, through: :locations
+  has_many :assessments, through: :resources
+
+
   # Creation will be logged during user creation.
   after_update :log_update
   after_destroy :log_destroy
