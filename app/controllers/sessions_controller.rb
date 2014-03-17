4693f0f28af91eaae764e45648b6657177baab74
Alex Dolski <alexd@n-ary.net>
2014-03-17 10:16:43 -0500

    Sign-in & out are logged to the event log

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,11 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:session][:username].downcase)
     if user && user.confirmed && user.authenticate(params[:session][:password])
       sign_in user
+
+      # Log the successful signin
+      Event.create(description: "User #{user.username} signed in",
+                   user: user)
+
       redirect_back_or user
     else
       sleep 2 # slow down brute-force attacks
@@ -17,7 +22,14 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    user = current_user
+
     sign_out
+
+    # Log the signout
+    Event.create(description: "User #{user.username} signed out",
+                 user: user)
+
     redirect_to root_url
   end
 
