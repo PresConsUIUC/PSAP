4cc0ae5d155cde455a71fe205d0df767b92990b2
Alex Dolski <alexd@n-ary.net>
2014-03-21 09:33:16 -0500

    Bugfix

--- app/models/format.rb
+++ app/models/format.rb
@@ -2,7 +2,6 @@ class Format < ActiveRecord::Base
   has_many :resources, inverse_of: :format
 
   validates :name, presence: true
-  validates :obsolete, presence: true
   validates :score, presence: true, numericality: {
       greater_than_or_equal_to: 0, less_than_or_equal_to: 1 }
 end
