fbaff627b3f9883d9b608f5fb9600b5a59d895d0
Alex Dolski <alexd@n-ary.net>
2014-03-21 13:29:59 -0500

    Update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 # formats
+Format.create!(name: 'THESE FORMATS HAVE NOT BEEN FINALIZED', score: 1, obsolete: false)
 Format.create!(name: '35mm nitrate', score: 0, obsolete: true)
 Format.create!(name: '35mm acetate', score: 0.375, obsolete: true)
 Format.create!(name: '35mm polyester', score: 0.875, obsolete: true)
@@ -25,53 +26,33 @@ case Rails.env
     # Admin role
     admin_role = Role.create!(name: 'Administrator', is_admin: true)
 
-    inst_admin_role = Role.create!(name: 'Institution Administrator', is_admin: false)
-
     # Normal role
     normal_role = Role.create!(name: 'User', is_admin: false)
 
     # Admin user
     admin_user = User.create!(username: 'admin', email: 'admin@example.org',
-                              first_name: 'System', last_name: 'Administrator',
-                              password: 'admin!', password_confirmation: 'admin!',
+                              first_name: 'Addison', last_name: 'McAdmin',
+                              password: 'password', password_confirmation: 'password',
                               institution: institution1, role: admin_role,
                               confirmed: true, enabled: true)
-
-    # Institution admin user
-    inst_admin_user = User.create!(username: 'inst', email: 'inst_admin@example.org',
-                                   first_name: 'Institution', last_name: 'Administrator',
-                                   password: 'inst_admin', password_confirmation: 'inst_admin',
-                                   institution: institution1, role: inst_admin_role,
-                                   confirmed: true, enabled: true)
-
     # Normal user
     normal_user = User.create!(username: 'normal', email: 'normal@example.org',
-                               first_name: 'Normal', last_name: 'User',
-                               password: 'normal', password_confirmation: 'normal',
+                               first_name: 'Norm', last_name: 'McNormalUser',
+                               password: 'password', password_confirmation: 'password',
                                institution: institution1, role: normal_role,
                                confirmed: true, enabled: true)
 
-    # Alex
-    alex_user = User.create!(username: 'alexd', email: 'alexd@illinois.edu',
-                             first_name: 'Alex', last_name: 'Dolski',
-                             password: 'password', password_confirmation: 'password',
-                             institution: institution1, role: admin_role,
-                             confirmed: true, enabled: true)
-
-    # Ryan
-    ryan_user = User.create!(username: 'edge2', email: 'edge2@illinois.edu',
-                             first_name: 'Ryan', last_name: 'Edge',
-                             password: 'password', password_confirmation: 'password',
-                             institution: institution1, role: admin_role,
-                             confirmed: true, enabled: true)
-
     repository = Repository.create!(institution: institution1,
-                                    name: 'First Repository')
+                                    name: 'Sample Repository')
+    repository2 = Repository.create!(institution: institution1,
+                                    name: 'Another Sample Repository')
 
     location = Location.create!(name: 'Secret Location', repository: repository)
-    location2 = Location.create!(name: 'Super-Secret Location', repository: repository)
-    location3 = Location.create!(name: 'Super-Duper-Secret Location', repository: repository)
-    location4 = Location.create!(name: 'Public Location', repository: repository)
+    location2 = Location.create!(name: 'Over there by the file cabinet', repository: repository)
+    location3 = Location.create!(name: 'Back Room', repository: repository)
+    location4 = Location.create!(name: 'Front Room', repository: repository)
+    location5 = Location.create!(name: 'Side Room', repository: repository)
+    location6 = Location.create!(name: 'Hidden Vault', repository: repository)
 
     resource = Resource.create(name: 'Magna Carta',
                                resource_type: ResourceType::ITEM,
@@ -82,49 +63,47 @@ case Rails.env
     resource3 = Resource.create(name: 'Sears Catalog Collection',
                                 resource_type: ResourceType::COLLECTION,
                                 location: location2)
-    resource4 = Resource.create(name: 'JC Penney Catalog Collection',
+    resource4 = Resource.create(name: 'My Old Baseball Card Collection',
                                 resource_type: ResourceType::COLLECTION,
                                 location: location2)
-    resource5 = Resource.create(name: 'U.S. Constitution',
+    resource5 = Resource.create(name: 'Treaty of Verdun',
                                 resource_type: ResourceType::ITEM,
                                 location: location3)
-    resource6 = Resource.create(name: 'Declaration of Independence',
+    resource6 = Resource.create(name: 'Declaration of Paris',
                                 resource_type: ResourceType::ITEM,
                                 location: location3)
     resource7 = Resource.create(name: 'Cat Fancy Collection',
                                 resource_type: ResourceType::COLLECTION,
                                 location: location4)
-    resource8 = Resource.create(name: 'Tiger Beat Collection',
-                                resource_type: ResourceType::COLLECTION,
-                                location: location4)
 
     permission = Permission.create!(key: 'institutions.edit_own')
-    inst_admin_role.permissions << permission
+    admin_role.permissions << permission
 
     permission = Permission.create!(key: 'repositories.create_own')
-    inst_admin_role.permissions << permission
+    admin_role.permissions << permission
 
     permission = Permission.create!(key: 'repositories.edit_own')
-    inst_admin_role.permissions << permission
+    admin_role.permissions << permission
 
     permission = Permission.create!(key: 'repositories.delete_own')
-    inst_admin_role.permissions << permission
+    admin_role.permissions << permission
 
     permission = Permission.create!(key: 'resources.create_own')
-    inst_admin_role.permissions << permission
+    admin_role.permissions << permission
 
     permission = Permission.create!(key: 'resources.edit_own')
-    inst_admin_role.permissions << permission
+    admin_role.permissions << permission
 
     permission = Permission.create!(key: 'resources.delete_own')
-    inst_admin_role.permissions << permission
+    admin_role.permissions << permission
 
     permission = Permission.create!(key: 'users.view_all')
-    inst_admin_role.permissions << permission
+    admin_role.permissions << permission
 
     permission = Permission.create!(key: 'users.view_all_in_own_institution')
-    inst_admin_role.permissions << permission
-
-  else # test or production
+    admin_role.permissions << permission
 
 end
+
+# Most of the above commands generated events, which we don't want.
+Event.destroy_all
