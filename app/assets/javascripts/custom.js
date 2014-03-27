2d54091ffa3f5a5acac52e989c2d393395d697ad
Alex Dolski <alexd@n-ary.net>
2014-03-27 14:43:23 -0500

    Improve client-side registration form validation

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -68,27 +68,99 @@ function FauxMultiPageView() {
 var UserForm = {
 
     attachEventListeners: function() {
-        $('input#user_password').bind('keyup input paste', function() {
-            UserForm.refreshPasswordStatus();
-            UserForm.refreshPasswordConfirmationStatus();
+        $('input#user_first_name').bind('keyup input paste', function() {
+            UserForm.validateFirstName();
         });
-        $('input#user_password_confirmation').bind('keyup input paste', function() {
-            UserForm.refreshPasswordConfirmationStatus();
+        $('input#user_last_name').bind('keyup input paste', function() {
+            UserForm.validateLastName();
+        });
+        $('input#user_email').bind('keyup input paste', function() {
+            UserForm.validateEmail();
         });
         $('input#user_username').bind('keyup input paste', function() {
-            UserForm.checkUsername();
+            UserForm.validateUsername();
+        });
+        $('input#user_password').bind('keyup input paste', function() {
+            UserForm.validatePassword();
+            UserForm.validatePasswordConfirmation();
+        });
+        $('input#user_password_confirmation').bind('keyup input paste', function() {
+            UserForm.validatePasswordConfirmation();
         });
     },
 
-    checkUsername: function() {
+    validateFirstName: function() {
+        var input = $('input#user_first_name');
+        input.parent('div').removeClass('has-success');
+        input.parent('div').removeClass('has-error');
+        input.next('span').removeClass('glyphicon-ok');
+        input.next('span').removeClass('glyphicon-remove');
+
+        var value = input.val().trim();
+        if (value.length && value.length <= 255) {
+            input.parent('div').addClass('has-success');
+            input.next('span').addClass('glyphicon-ok');
+        } else {
+            input.parent('div').addClass('has-error');
+            input.next('span').addClass('glyphicon-remove');
+        }
+    },
+
+    validateLastName: function() {
+        var input = $('input#user_last_name');
+        input.parent('div').removeClass('has-success');
+        input.parent('div').removeClass('has-error');
+        input.next('span').removeClass('glyphicon-ok');
+        input.next('span').removeClass('glyphicon-remove');
+
+        var value = input.val().trim();
+        if (value.length && value.length <= 255) {
+            input.parent('div').addClass('has-success');
+            input.next('span').addClass('glyphicon-ok');
+        } else {
+            input.parent('div').addClass('has-error');
+            input.next('span').addClass('glyphicon-remove');
+        }
+    },
+
+    validateEmail: function() {
+        var input = $('input#user_email');
+        input.parent('div').removeClass('has-success');
+        input.parent('div').removeClass('has-error');
+        input.next('span').removeClass('glyphicon-ok');
+        input.next('span').removeClass('glyphicon-remove');
+
+        var value = input.val().trim();
+
+        // Far from perfect, but forces the user to at least put in the effort
+        // to make it look real.
+        var regex = /\S+@\S+\.\S+/;
+
+        if (regex.test(value)) {
+            input.parent('div').addClass('has-success');
+            input.next('span').addClass('glyphicon-ok');
+        } else {
+            input.parent('div').addClass('has-error');
+            input.next('span').addClass('glyphicon-remove');
+        }
+    },
+
+    validateUsername: function() {
         var msg_element = $('p#username_status');
 
         var username = $('input#user_username').val().trim();
         if (!username) {
             msg_element.text('');
+            msg_element.removeClass('text-success');
+            msg_element.removeClass('text-danger');
+            msg_element.parent('div').removeClass('has-success');
+            msg_element.parent('div').removeClass('has-error');
+            msg_element.prev('span').removeClass('glyphicon-ok');
+            msg_element.prev('span').removeClass('glyphicon-remove');
             return;
         }
 
+        // window.pageURL is set in users/new.html.erb
         var url = window.pageURL + 'users/' + username + '/exists';
 
         $.ajax(url, {
@@ -99,11 +171,23 @@ var UserForm = {
                     msg_element.removeClass('text-success');
                     msg_element.addClass('text-danger');
                     msg_element.text('Username is in use.');
+
+                    msg_element.parent('div').removeClass('has-success');
+                    msg_element.parent('div').addClass('has-error');
+
+                    msg_element.prev('span').removeClass('glyphicon-ok');
+                    msg_element.prev('span').addClass('glyphicon-remove');
                 },
                 404: function(response) {
                     msg_element.addClass('text-success');
                     msg_element.removeClass('text-danger');
-                    msg_element.text('Username is available.');
+                    msg_element.text('');
+
+                    msg_element.parent('div').addClass('has-success');
+                    msg_element.parent('div').removeClass('has-error');
+
+                    msg_element.prev('span').addClass('glyphicon-ok');
+                    msg_element.prev('span').removeClass('glyphicon-remove');
                 }
             },
             success: function() {
@@ -111,44 +195,56 @@ var UserForm = {
         });
     },
 
-    refreshPasswordStatus: function() {
+    validatePassword: function() {
         var MIN_LENGTH = 6;
 
-        // Update the password feedback text
-        var value = $('input#user_password').val();
-        var message = '';
+        var input = $('input#user_password');
+        input.parent('div').removeClass('has-success');
+        input.parent('div').removeClass('has-error');
+        input.next('span').removeClass('glyphicon-ok');
+        input.next('span').removeClass('glyphicon-remove');
 
-        $('div.progress-bar').attr('aria-valuenow', value.length / MIN_LENGTH);
-        $('div.progress-bar').width(((value.length / MIN_LENGTH) * 100) + '%');
+        var value = input.val();
+
+        var progress_bar = $('div.progress-bar');
+        progress_bar.attr('aria-valuenow', value.length / MIN_LENGTH);
+        progress_bar.width(((value.length / MIN_LENGTH) * 100) + '%');
 
         if (value.length > 5) {
-            $('div.progress-bar').removeClass('progress-bar-warning');
-            $('div.progress-bar').addClass('progress-bar-success');
+            input.parent('div').addClass('has-success');
+            input.next('span').addClass('glyphicon-ok');
+            progress_bar.removeClass('progress-bar-warning');
+            progress_bar.addClass('progress-bar-success');
         } else {
-            $('div.progress-bar').addClass('progress-bar-warning');
-            $('div.progress-bar').removeClass('progress-bar-success');
+            input.parent('div').addClass('has-error');
+            input.next('span').addClass('glyphicon-remove');
+            progress_bar.addClass('progress-bar-warning');
+            progress_bar.removeClass('progress-bar-success');
         }
-
-        if (value.length == 5) {
-            message = 'Needs 1 more character.';
-        } else if (value.length < MIN_LENGTH) {
-            message = 'Needs ' + (MIN_LENGTH - value.length) + ' more characters.';
-        }
-        //$('#password_status').text(message);
     },
 
-    refreshPasswordConfirmationStatus: function() {
+    validatePasswordConfirmation: function() {
+        var input = $('input#user_password_confirmation');
+        input.parent('div').removeClass('has-success');
+        input.parent('div').removeClass('has-error');
+        input.next('span').removeClass('glyphicon-ok');
+        input.next('span').removeClass('glyphicon-remove');
+
         var value = $('input#user_password').val();
-        var confirmation_value = $('input#user_password_confirmation').val();
+        var confirmation_value = input.val();
         var status_p = $('p#password_confirmation_status');
         var message = '';
 
         if (value == confirmation_value) {
+            input.parent('div').addClass('has-success');
+            input.next('span').addClass('glyphicon-ok');
             status_p.addClass('text-success');
-            status_p.removeClass('text-warning');
+            status_p.removeClass('text-danger');
         } else {
+            input.parent('div').addClass('has-error');
+            input.next('span').addClass('glyphicon-remove');
             message = 'Passwords don\'t match.';
-            status_p.addClass('text-warning');
+            status_p.addClass('text-danger');
             status_p.removeClass('text-success');
         }
 
