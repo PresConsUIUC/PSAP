05bf0a98efd4cd7903d734f53c40cb6b9f3a5d83
Alex Dolski <alexd@n-ary.net>
2014-04-02 13:39:07 -0500

    Add max length & uniqueness validations

--- app/models/format.rb
+++ app/models/format.rb
@@ -1,7 +1,8 @@
 class Format < ActiveRecord::Base
   has_many :resources, inverse_of: :format
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 255 },
+            uniqueness: { case_sensitive: false }
   validates :score, presence: true, numericality: {
       greater_than_or_equal_to: 0, less_than_or_equal_to: 1 }
 end
