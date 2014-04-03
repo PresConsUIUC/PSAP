d54dfa25fc8c16022925dacc4a1762356abe6d1f
Alex Dolski <alexd@n-ary.net>
2014-04-03 11:04:48 -0500

    "My resource assessments in progress" and "recently added assessments" panes work

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,6 +7,10 @@ class DashboardController < ApplicationController
     @institution_users = @user.institution ?
         @user.institution.users.where('id not in (?)', @user.id).order(:last_name) :
         nil
+    @assessments = current_user.resources.order(:name)
+    @recent_assessments = Resource.joins(:location => { :repository => :institution }).
+        where(:institutions => { :id => @user.institution.id }).
+        order(:created_at => :desc).limit(5)
   end
 
 end
