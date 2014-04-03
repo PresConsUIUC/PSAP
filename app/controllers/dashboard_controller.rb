3a580b896e0ac250cda5936586892ed828a5000b
Alex Dolski <alexd@n-ary.net>
2014-04-03 12:36:20 -0500

    Dashboard improvements/fixes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,13 +4,19 @@ class DashboardController < ApplicationController
 
   def index
     @user = current_user
-    @institution_users = @user.institution ?
-        @user.institution.users.where('id not in (?)', @user.id).order(:last_name) :
-        nil
+
     @assessments = current_user.resources.order(:name)
-    @recent_assessments = Resource.joins(:location => { :repository => :institution }).
-        where(:institutions => { :id => @user.institution.id }).
-        order(:created_at => :desc).limit(5)
+
+    if @user.institution
+      @institution_users = @user.institution.users.
+          where('id not in (?)', @user.id).order(:last_name)
+      @recent_assessments = Resource.joins(:location => { :repository => :institution }).
+          where(:institutions => { :id => @user.institution.id }).
+          order(:created_at => :desc).limit(5)
+    else
+      @institution_users = nil
+      @recent_assessments = nil
+    end
   end
 
 end
