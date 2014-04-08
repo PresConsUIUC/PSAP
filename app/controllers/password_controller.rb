750096a7559059e0dac50fd9690f57d456934cbd
Alex Dolski <alexd@n-ary.net>
2014-04-08 15:41:44 -0500

    Add PasswordControllerTest

--- app/controllers/password_controller.rb
+++ app/controllers/password_controller.rb
@@ -7,34 +7,39 @@ class PasswordController < ApplicationController
 
   # Responds to POST /forgot_password
   def send_email
-    @user = User.find_by_username params[:username]
-    if @user
-      @user.password_reset_key = SecureRandom.urlsafe_base64(nil, false)
-      @user.save!
-      UserMailer.welcome_email(@user).deliver
-      flash[:success] = 'An email has been sent containing a link to reset
-                        your password.'
+    if params[:user]
+      @user = User.find_by_username params[:user][:username]
+      if @user
+        @user.password_reset_key = SecureRandom.urlsafe_base64(nil, false)
+        @user.save!
+        UserMailer.welcome_email(@user).deliver unless Rails.env.test?
+        flash[:notice] = 'An email has been sent containing a link to reset '\
+                          'your password.'
+      end
     end
     redirect_to root_url
   end
 
   # Responds to GET /new_password, which handles incoming links from password
-  # reset emails.
+  # reset emails, and contains a form that will POST to /new_password.
   def new_password
     @user = User.find_by_username params[:username]
-    if !@user || params[:key] != @user.password_reset_key
+    if !@user || @user.password_reset_key.nil? ||
+        params[:key] != @user.password_reset_key
       redirect_to root_url
     end
   end
 
   # Responds to POST /new_password
   def reset_password
-    @user = User.find_by_username params[:username]
-    if @user
-      @user.password_reset_key = nil;
-      @user.update_attributes(user_change_password_params)
-      flash[:success] = 'Password reset successfully.'
-      redirect_to signin_url
+    if params[:user]
+      @user = User.find_by_username params[:user][:username]
+      if @user && params[:user][:password_reset_key] == @user.password_reset_key
+        @user.password_reset_key = nil;
+        @user.update_attributes(user_change_password_params)
+        flash[:success] = 'Password reset successfully.'
+        return redirect_to signin_url
+      end
     end
     redirect_to root_url
   end
@@ -42,7 +47,8 @@ class PasswordController < ApplicationController
   private
 
   def user_change_password_params
-    params.require(:user).permit(:password, :password_confirmation)
+    params.require(:user).permit(:username, :password, :password_confirmation,
+                                 :password_reset_key)
   end
 
 end
---
+++ test/controllers/password_controller_test.rb
@@ -0,0 +1,105 @@
+require 'test_helper'
+
+class PasswordControllerTest < ActionController::TestCase
+
+  #### forgot_password ####
+
+  test 'forgot-password page should display' do
+    get :forgot_password
+    assert_response :success
+  end
+
+  #### send_email ####
+
+  test 'send_email should redirect to root url if no user provided' do
+    post :send_email
+    assert_redirected_to root_url
+  end
+
+  test 'send_email should redirect to root url if invalid user provided' do
+    post :send_email, post: { username: 'asdflkjsafdljk' }
+    assert_redirected_to root_url
+  end
+
+  test 'send_email should do what it\'s supposed to' do
+    post :send_email, user: { username: 'normal' }
+
+    assert_not_nil users(:normal_user).password_reset_key
+    assert_equal 'An email has been sent containing a link to reset your password.',
+                 flash[:notice]
+    assert_redirected_to root_url
+  end
+
+  #### new_password ####
+
+  test 'new-password page should redirect to root url if no user provided' do
+    get :new_password
+    assert_redirected_to root_url
+  end
+
+  test 'new-password page should redirect to root url if invalid user provided' do
+    get :new_password, { username: 'adflajsflaskjfaslf' }
+    assert_redirected_to root_url
+  end
+
+  test 'new-password page should redirect to root url if user doesn\'t have a password reset key' do
+    get :new_password, { username: 'normal', key: 'cats' }
+    assert_redirected_to root_url
+  end
+
+  test 'new-password page should redirect to root url if invalid password reset key provided' do
+    users(:normal_user).password_reset_key = 'cats'
+    users(:normal_user).save!
+    get :new_password, { username: 'normal', key: 'adlfkjasdflkjasdf' }
+    assert_redirected_to root_url
+  end
+
+  test 'new-password page should redirect to root url if password reset key not provided' do
+    users(:normal_user).password_reset_key = 'cats'
+    users(:normal_user).save!
+    get :new_password, { username: 'normal' }
+    assert_redirected_to root_url
+  end
+
+  test 'new-password page should display if valid user and key provided' do
+    users(:normal_user).password_reset_key = 'cats'
+    users(:normal_user).save!
+    get :new_password, { username: 'normal', key: 'cats' }
+    assert_response :success
+  end
+
+  #### reset_password
+
+  test 'reset-password page should redirect to root url if no user provided' do
+    post :reset_password
+    assert_redirected_to root_url
+  end
+
+  test 'reset-password page should redirect to root url if invalid user provided' do
+    post :reset_password, user: { username: 'dsadfasfd' }
+    assert_redirected_to root_url
+  end
+
+  test 'reset-password page should redirect to root url if invalid password reset key provided' do
+    users(:normal_user).password_reset_key = 'cats'
+    users(:normal_user).save!
+    post :reset_password, user: { username: 'dsfasdffd',
+                                  password_reset_key: 'afdkjsfasdf' }
+    assert_redirected_to root_url
+  end
+
+  test 'reset-password page should redirect to root url if user has no password reset key' do
+    post :reset_password, user: { username: 'dsfasdffd',
+                                  password_reset_key: 'cats' }
+    assert_redirected_to root_url
+  end
+
+  test 'reset_password should do what it\'s supposed to' do
+    post :reset_password, user: { username: 'normal' }
+
+    assert_nil users(:normal_user).password_reset_key
+    assert_equal 'Password reset successfully.', flash[:success]
+    assert_redirected_to signin_url
+  end
+
+end
