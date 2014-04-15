11ad88fc27009afdab2a726b2c8f16d47607bafa
Alex Dolski <alexd@n-ary.net>
2014-04-15 16:45:47 -0500

    Fix description param not being allowed

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -67,7 +67,7 @@ class LocationsController < ApplicationController
   end
 
   def location_params
-    params.require(:location).permit(:name, :repository)
+    params.require(:location).permit(:name, :description, :repository)
   end
 
 end
