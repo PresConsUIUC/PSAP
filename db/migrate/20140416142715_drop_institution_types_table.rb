6368f9fc6ff105122e2d148a2b696306663187e6
Alex Dolski <alexd@n-ary.net>
2014-04-18 14:48:01 -0500

    Fix a potential migration error

--- db/migrate/20140416142715_drop_institution_types_table.rb
+++ db/migrate/20140416142715_drop_institution_types_table.rb
@@ -1,5 +1,5 @@
 class DropInstitutionTypesTable < ActiveRecord::Migration
   def change
-    drop_table :institution_types
+    drop_table :institution_types if table_exists? :institution_types
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140416142715) do
+ActiveRecord::Schema.define(version: 20140417190822) do
 
   create_table "creators", force: true do |t|
     t.string   "name"
