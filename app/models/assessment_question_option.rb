aa86f54983ff290d76a93940993f6d0a1bb2e152
Alex Dolski <alexd@n-ary.net>
2014-04-23 14:31:16 -0500

    assessment_question is required

--- app/models/assessment_question_option.rb
+++ app/models/assessment_question_option.rb
@@ -1,3 +1,5 @@
 class AssessmentQuestionOption < ActiveRecord::Base
   belongs_to :assessment_question, inverse_of: :assessment_question_options
+
+  validates :assessment_question, presence: true
 end
