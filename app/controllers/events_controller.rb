3b0ea40bbaeecf794de0a8fbaf0742ef70945b5f
Alex Dolski <alexd@n-ary.net>
2014-04-28 10:28:03 -0500

    Add live search to events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,8 +3,10 @@ class EventsController < ApplicationController
   before_action :signed_in_user, :admin_user
 
   def index
-    @events = Event.order(created_at: :desc).paginate(page: params[:page],
-                                                      per_page: 100)
+    q = "%#{params[:q]}%"
+    @events = Event.joins(:user).where(
+        'events.description LIKE ? OR users.username LIKE ? OR events.address LIKE ?', q, q, q).
+        order(created_at: :desc).paginate(page: params[:page], per_page: 100)
     @user = current_user
   end
 
