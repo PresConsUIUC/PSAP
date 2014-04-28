6a8f0d2cfdf80a378ccfdab63b361aaf9a0cd304
Alex Dolski <alexd@n-ary.net>
2014-04-28 11:08:19 -0500

    Initial work on progress bar

--- app/assets/javascripts/resources.js
+++ app/assets/javascripts/resources.js
@@ -8,20 +8,22 @@ var ResourceForm = {
 
     init: function() {
         ResourceForm.attachEventListeners();
+        ResourceForm.updateProgressBar();
+        ResourceForm.updateScoreBar();
     },
 
     attachEventListeners: function() {
         $('select.date_type').on('change', function(event) {
             switch (parseInt(this.value)) {
                 case 0: // single
-                    $(this).nextAll('div.date:first').show()
-                    $(this).nextAll('div.begin_date:first').hide()
-                    $(this).nextAll('div.end_date:first').hide()
+                    $(this).nextAll('div.date:first').show();
+                    $(this).nextAll('div.begin_date:first').hide();
+                    $(this).nextAll('div.end_date:first').hide();
                     break;
                 default: // bulk or inclusive/span
-                    $(this).nextAll('div.date:first').hide()
-                    $(this).nextAll('div.begin_date:first').show()
-                    $(this).nextAll('div.end_date:first').show()
+                    $(this).nextAll('div.date:first').hide();
+                    $(this).nextAll('div.begin_date:first').show();
+                    $(this).nextAll('div.end_date:first').show();
                     break;
             }
         }).trigger('change');
@@ -42,6 +44,27 @@ var ResourceForm = {
                 $(this).parent().parent().find('input.day').prop('disabled', false);
             }
         }).trigger('keyup');
+
+        $('.question input, .question select').on('change', function(event) {
+            ResourceForm.updateProgressBar();
+            ResourceForm.updateScoreBar();
+        });
+    },
+
+    updateScoreBar: function() {
+    },
+
+    updateProgressBar: function() {
+        var numQuestions = $('.question').length;
+        var numAnsweredQuestions = 0;
+        $('.question').each(function() {
+            var numChecked = $(this).find('input:checked').length;
+            if (numChecked > 0 || $(this).find('select').val() !== undefined) {
+                numAnsweredQuestions++;
+            }
+        });
+        $('div.progress-bar.psap-progress').attr('style',
+                'width:' + numAnsweredQuestions / numQuestions * 100 + '%');
     }
 
 };
