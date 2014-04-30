382379c5e919d79ab6cf84f22ce88db0da91d361
Alex Dolski <alexd@n-ary.net>
2014-04-30 11:02:02 -0500

    Kill the assessment_options table that has once again risen from the dead

--- db/migrate/20140430154700_drop_assessment_options_table.rb
+++ db/migrate/20140430154700_drop_assessment_options_table.rb
@@ -1,5 +1,5 @@
 class DropAssessmentOptionsTable < ActiveRecord::Migration
   def change
-    drop_table :assessment_options
+    drop_table :assessment_options if table_exists? :assessment_options
   end
 end
