ec29a1166688f29f778464d978fb1ec8c03151b1
Alex Dolski <alexd@n-ary.net>
2014-05-06 09:03:23 -0500

    Restyle event levels

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,5 +1,28 @@
 module EventsHelper
 
+  def bootstrap_class_for_event_level level
+    case level
+      when EventLevel::DEBUG
+        'text-muted'
+      when EventLevel::INFO
+        'text-info'
+      when EventLevel::NOTICE
+        'text-info'
+      when EventLevel::WARNING
+        'text-warning'
+      when EventLevel::ERROR
+        'text-danger'
+      when EventLevel::CRITICAL
+        'text-danger'
+      when EventLevel::ALERT
+        'text-danger'
+      when EventLevel::EMERGENCY
+        'text-danger'
+      else
+        flash_type.to_s
+    end
+  end
+
   def level_buttons(current_level)
     levels = EventLevel::all.sort do |a, b|
       b <=> a
