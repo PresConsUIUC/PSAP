8889ac6eca1542a6c0e34559216769e4c221771e
Alex Dolski <alexd@n-ary.net>
2014-05-07 15:31:59 -0500

    Enable automatic code reloading in rails console

---
+++ config/initializers/irb_reloading.rb
@@ -0,0 +1,16 @@
+# Enables automatic code reloading in Rails console. From:
+# http://jkfill.com/2012/12/08/automatic-code-reloading-in-rails-console/
+
+if defined?(IRB::Context) && !defined?(Rails::Server) && Rails.env.development?
+  class IRB::Context
+    def evaluate_with_reloading(line, line_no)
+      ActionDispatch::Reloader.cleanup!
+      ActionDispatch::Reloader.prepare!
+
+      evaluate_without_reloading(line, line_no)
+    end
+    alias_method_chain :evaluate, :reloading
+  end
+
+  puts "=> IRB code reloading enabled"
+end
