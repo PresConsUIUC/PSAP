90fd7ab90e0dbfd86243d445782bb5faf85fa23f
Alex Dolski <alexd@n-ary.net>
2014-05-08 12:15:45 -0500

    Add DeleteLocationCommandTest

---
+++ test/commands/delete_location_command_test.rb
@@ -0,0 +1,48 @@
+require 'test_helper'
+
+class DeleteLocationCommandTest < ActiveSupport::TestCase
+
+  def setup
+    @location = locations(:location_one)
+
+    @user = users(:normal_user)
+    @remote_ip = '10.0.0.1'
+
+    @command = DeleteLocationCommand.new(@location, @user, @remote_ip)
+  end
+
+  # execute
+  test 'execute method should delete location' do
+    assert_nothing_raised do
+      @command.execute
+    end
+  end
+
+  test 'execute method should write success to event log if successful' do
+    assert_difference 'Event.count' do
+      assert_difference 'Location.count', -1 do
+        @command.execute
+      end
+    end
+    assert @location.destroyed?
+    event = Event.order(:created_at).last
+    assert_equal "Deleted location \"#{@location.name}\" from "\
+      "repository \"#{@location.repository.name}\"", event.description
+    assert_equal @user, event.user
+    assert_equal @remote_ip, event.address
+  end
+
+  test 'execute method should raise DeleteRestrictionError if a constraint prevents delete' do
+    skip 'Need to get this to happen'
+  end
+
+  test 'execute method should write failure to event log if unsuccessful' do
+    skip 'Need to get this to happen'
+  end
+
+  # object
+  test 'object method should return the deleted Location object' do
+    assert_kind_of Location, @command.object
+  end
+
+end
