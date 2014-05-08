1645d4154fe301966b309f34fafbbe281676b29e
Alex Dolski <alexd@n-ary.net>
2014-05-08 16:54:55 -0500

    Simplify feed data gathering

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -11,11 +11,7 @@ class DashboardController < ApplicationController
         @user_events = Event.where(user: @user).order(created_at: :desc).limit(limit)
 
         if @user.institution
-          @institution_events = Event.
-              joins('LEFT JOIN users ON users.id = events.user_id').
-              joins('LEFT JOIN institutions ON users.institution_id = institutions.id').
-              where('users.institution_id' => @user.id).
-              order(created_at: :desc).limit(5)
+          @institution_events = events_for_institution(@user, limit)
           @institution_users = @user.institution.users.
               where('id != ?', @user.id).order(:last_name)
           @recent_assessments = Resource.
@@ -33,7 +29,7 @@ class DashboardController < ApplicationController
         limit = 20
         @user = User.find_by_feed_key params[:key]
         if @user
-          @events = @user.institution ? all_events_for_user(@user limit) : []
+          @events = @user.institution ? events_for_institution(@user, limit) : []
         else
           render status: :forbidden, text: 'Access denied.'
           return
@@ -48,51 +44,14 @@ class DashboardController < ApplicationController
     request.format.atom?
   end
 
-  def recent_assessments_in_same_institution(user, limit)
-    events = []
-    assessments = Resource.
-        joins(location: { repository: :institution }).
-        where(institutions: { id: user.institution.id }).
-        order(updated_at: :desc).limit(limit)
-    assessments.each do |assessment|
-      events << {
-          id: "tag:#{request.host}:#{request.fullpath}:assessment:#{assessment.id}",
-          description: "Updated resource assessment \"#{assessment.name}\"",
-          timestamp: assessment.updated_at,
-          summary: "Updated resource assessment \"#{assessment.name}\"",
-          user: user
-      }
-    end
-    events
-  end
-
-  def recent_users_in_same_institution(user, limit)
-    events = []
-    users = User.
-        where(institution_id: @user.institution.id).
-        order(created_at: :desc).limit(limit)
-    users.each do |user|
-      events << {
-          id: "tag:#{request.host}:#{request.fullpath}:user:#{user.username}",
-          description: "New user at #{user.institution.name}: "\
-                  "#{user.full_name}",
-          timestamp: user.created_at,
-          summary: "New user at #{user.institution.name}: "\
-                  "#{user.full_name}",
-          user: user
-      }
-    end
-    events
-  end
-
   # Returns an array of generic event hashes.
-  def all_events_for_user(user, limit)
-    events = []
-    # TODO: institution event feed instead of these
-    events.concat recent_assessments_in_same_institution(user, limit)
-    events.concat recent_users_in_same_institution(user, limit)
-    events.sort! { |a, b| b[:timestamp] <=> a[:timestamp] }
-    events[0..limit]
+  def events_for_institution(user, limit)
+    events = Event.
+        joins('LEFT JOIN users ON users.id = events.user_id').
+        joins('LEFT JOIN institutions ON users.institution_id = institutions.id').
+        where('users.institution_id' => user.id).
+        order(created_at: :desc).limit(limit)
+    events
   end
 
 end
--- app/views/dashboard/index.atom.builder
+++ app/views/dashboard/index.atom.builder
@@ -10,7 +10,7 @@ xml.feed(
   xml.title('My PSAP Dashboard')
   xml.link('href' => events_url)
   if @events.any?
-    xml.updated(@events[0][:timestamp].iso8601)
+    xml.updated(@events[0].created_at.iso8601)
   end
   xml.author {
     xml.name('Preservation Self-Assessment Program (PSAP)')
@@ -24,16 +24,16 @@ xml.feed(
 
   @events.each do |event|
     xml.entry {
-      xml.title(event[:description])
+      xml.title(event.description)
       xml.author {
-        xml.name("#{event[:user].full_name} (#{event[:user].username})")
-        xml.email(event[:user].email)
-        xml.uri(user_url(event[:user]))
+        xml.name("#{event.user.full_name} (#{event.user.username})")
+        xml.email(event.user.email)
+        xml.uri(user_url(event.user))
       }
       xml.link('href' => dashboard_url, 'rel' => 'via')
-      xml.id(event[:id])
-      xml.updated(event[:timestamp].iso8601)
-      xml.summary(event[:summary])
+      xml.id(event.id)
+      xml.updated(event.created_at.iso8601)
+      xml.summary(event.description)
     }
   end
 }
