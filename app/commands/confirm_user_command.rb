21113bbdce9b527274dfadc167b82fba55ae3fdf
Alex Dolski <alexd@n-ary.net>
2014-05-09 14:05:01 -0500

    Remove a pointless check

--- app/commands/confirm_user_command.rb
+++ app/commands/confirm_user_command.rb
@@ -7,25 +7,23 @@ class ConfirmUserCommand < Command
   end
 
   def execute
-    if @user
-      if !@user.confirmed && @confirmation_code == @user.confirmation_code
-        @user.confirmed = true
-        @user.confirmation_code = nil
-        @user.enabled = true
-        @user.save!
-        Event.create(description: "Confirmed user #{@user.username}",
-                     user: @user, address: @remote_ip)
-      elsif @confirmation_code != @user.confirmation_code
-        Event.create(description: "Invalid confirmation code supplied for user "\
-        "#{@user.username}",
-                     user: @user, address: @remote_ip,
-                     event_level: EventLevel::ERROR)
-      else
-        Event.create(description: "User #{@user.username} attempted to "\
-        "confirm account, but was already confirmed.",
-                     user: @user, address: @remote_ip,
-                     event_level: EventLevel::NOTICE)
-      end
+    if !@user.confirmed && @confirmation_code == @user.confirmation_code
+      @user.confirmed = true
+      @user.confirmation_code = nil
+      @user.enabled = true
+      @user.save!
+      Event.create(description: "Confirmed user #{@user.username}",
+                   user: @user, address: @remote_ip)
+    elsif @confirmation_code != @user.confirmation_code
+      Event.create(description: "Invalid confirmation code supplied for user "\
+      "#{@user.username}",
+                   user: @user, address: @remote_ip,
+                   event_level: EventLevel::ERROR)
+    else
+      Event.create(description: "User #{@user.username} attempted to "\
+      "confirm account, but was already confirmed.",
+                   user: @user, address: @remote_ip,
+                   event_level: EventLevel::NOTICE)
     end
   end
 
