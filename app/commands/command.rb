d520ca4321b43cdc43fe490c64ed79355129f885
Alex Dolski <alexd@n-ary.net>
2014-05-09 14:04:26 -0500

    Add documentation

--- app/commands/command.rb
+++ app/commands/command.rb
@@ -1,12 +1,18 @@
-# Abstract command class from which all commands should inherit.
+# Abstract command class in the command pattern, from which all commands
+# should inherit.
 class Command
 
-  # Executes the command, raising an exception if anything goes wrong.
+  # Executes the command, raising an exception if anything goes wrong. Raised
+  # exception messages should be suitable for public consumption.
+  # Success/failure should be written to the event log.
   def execute
     raise NotImplementedError, 'Command must override execute method'
   end
 
-  # Returns the object, if any, on which the command will or did act.
+  # Returns the object, if any, on which the command will or did act. This
+  # method exists to establish convention, but is optional. Some commands may
+  # operate on <> 1 object, in which case they might want to define their own
+  # getter(s) instead of this.
   def object
     nil
   end
