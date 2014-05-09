7b9ae89060e04ba8bacfba5571043de410288153
Alex Dolski <alexd@n-ary.net>
2014-05-09 09:42:20 -0500

    Add DeleteUserCommandTest

---
+++ test/commands/delete_user_command_test.rb
@@ -0,0 +1,43 @@
+require 'test_helper'
+
+class DeleteUserCommandTest < ActiveSupport::TestCase
+
+  def setup
+    @user = users(:normal_user)
+
+    @doing_user = users(:admin_user)
+    @remote_ip = '10.0.0.1'
+
+    @command = DeleteUserCommand.new(@user, @doing_user, @remote_ip)
+  end
+
+  # execute
+  test 'execute method should delete user' do
+    assert_nothing_raised do
+      @command.execute
+    end
+  end
+
+  test 'execute method should write success to event log if successful' do
+    assert_difference 'Event.count' do
+      assert_difference 'User.count', -1 do
+        @command.execute
+      end
+    end
+    assert @user.destroyed?
+    event = Event.order(:created_at).last
+    assert_equal "Deleted user #{@user.username}", event.description
+    assert_equal @doing_user, event.user
+    assert_equal @remote_ip, event.address
+  end
+
+  test 'execute method should write failure to event log if unsuccessful' do
+    skip 'Need to get this to happen'
+  end
+
+  # object
+  test 'object method should return the deleted User object' do
+    assert_kind_of User, @command.object
+  end
+
+end
