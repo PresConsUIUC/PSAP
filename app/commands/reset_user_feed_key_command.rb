d9e2a8df2561e5ee958046d9a1a40d5b6dd4ee90
Alex Dolski <alexd@n-ary.net>
2014-05-28 10:01:33 -0500

    Add ResetUserFeedKeyCommandTest

--- app/commands/reset_user_feed_key_command.rb
+++ app/commands/reset_user_feed_key_command.rb
@@ -8,9 +8,14 @@ class ResetUserFeedKeyCommand < Command
 
   def execute
     begin
+      # Fail if a non-admin user is trying to reset someone else's feed key
+      if !@doing_user.is_admin? && @user != @doing_user
+        raise 'Insufficient privileges'
+      end
+
       @user.reset_feed_key
       @user.save!
-      UserMailer.changed_feed_key_email(@user).deliver if @user != doing_user
+      UserMailer.changed_feed_key_email(@user).deliver if @user != @doing_user
     rescue => e
       Event.create(description: "Attempted to reset feed key for user "\
       "#{@user.username}, but failed: #{e.message}",
---
+++ test/commands/reset_user_feed_key_command_test.rb
@@ -0,0 +1,58 @@
+require 'test_helper'
+
+class ResetUserFeedKeyCommandTest < ActiveSupport::TestCase
+
+  def setup
+    @user = users(:normal_user)
+    @doing_user = users(:admin_user)
+    @remote_ip = '10.0.0.1'
+
+    @command = ResetUserFeedKeyCommand.new(@user, @doing_user, @remote_ip)
+  end
+
+  # execute
+  test 'execute method should reset feet key' do
+    key = @user.feed_key.dup
+    assert_nothing_raised do
+      @command.execute
+    end
+    assert_not_equal key, @user.feed_key
+  end
+
+  test 'execute method should write success to event log if successful' do
+    assert_difference 'Event.count' do
+      @command.execute
+    end
+    event = Event.order(:created_at).last
+    assert_equal "Reset feed key for user #{@user.username}", event.description
+    assert_equal @doing_user, event.user
+    assert_equal @remote_ip, event.address
+  end
+
+  test 'execute method should write failure to event log if unsuccessful' do
+    skip 'Need to get this to happen'
+  end
+
+  test 'non-admin users should not be able to reset other users\' feed keys' do
+    @doing_user = users(:normal_user)
+    @user = users(:admin_user)
+    key = @user.feed_key.dup
+    @command = ResetUserFeedKeyCommand.new(@user, @doing_user, @remote_ip)
+    assert_raises RuntimeError do
+      @command.execute
+    end
+    assert_equal key, @user.feed_key
+    event = Event.order(:created_at).last
+    assert_equal "Attempted to reset feed key for user #{@user.username}, but "\
+    "failed: Insufficient privileges",
+                 event.description
+    assert_equal @doing_user, event.user
+    assert_equal @remote_ip, event.address
+  end
+
+  # object
+  test 'object method should return the User object' do
+    assert_kind_of User, @command.object
+  end
+
+end
