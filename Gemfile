d69e83ce36c56c3d4280fdc7a21ad977b4d06341
Alex Dolski <alexd@n-ary.net>
2014-06-04 13:30:20 -0500

    Work on Capistrano deployment

--- Gemfile
+++ Gemfile
@@ -53,11 +53,15 @@ gem 'local_time'
 # Use unicorn as the app server
 # gem 'unicorn'
 
-# Use Capistrano for deployment
-# gem 'capistrano', group: :development
+gem 'capistrano', '~> 3.1.0'
+gem 'capistrano-rails', '~> 1.1.0'
+gem 'capistrano-bundler'
+gem 'capistrano-rbenv', '~> 2.0'
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
 
 gem 'nokogiri'
 
+# PostgreSQL
+gem 'pg'
---
+++ config/deploy.rb
@@ -0,0 +1,67 @@
+# config valid only for Capistrano 3.1
+lock '3.1.0'
+
+set :application, 'psap'
+set :deploy_user, 'psap'
+set :repo_url, 'git@github.com:PresConsUIUC/PSAP.git'
+
+# Default branch is :master
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+# Default deploy_to directory is /var/www/my_app
+set :deploy_to, '/home/psap/apps/psap'
+
+# Default value for :scm is :git
+set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+set :linked_files, %w{config/database.yml}
+
+# Default value for linked_dirs is []
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.1.1'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
+
+namespace :deploy do
+
+  before :deploy, 'deploy:check_revision'
+  after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
+  after :publishing, :restart
+  after :finishing, 'deploy:cleanup'
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      within release_path do
+        execute :rake, 'cache:clear'
+      end
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+end
