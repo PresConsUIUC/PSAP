80ea3623ea888b07396e7b9e6e7cf8e7e39ca866
Alex Dolski <alexd@n-ary.net>
2014-06-05 10:10:35 -0500

    Fix a problematic migration

--- db/migrate/20140425160626_change_type_of_assessment_question_option_value.rb
+++ db/migrate/20140425160626_change_type_of_assessment_question_option_value.rb
@@ -1,8 +1,5 @@
 class ChangeTypeOfAssessmentQuestionOptionValue < ActiveRecord::Migration
   def change
-    if column_exists? :assessment_question_options, :value
-      change_column :assessment_question_options, :value,
-                    'float USING CAST(value AS float)'
-    end
+    change_column :assessment_question_options, :value, :float
   end
 end
