e7f830c62216e93a7e5968ff0104827ee344813b
Alex Dolski <alexd@n-ary.net>
2014-06-04 21:54:49 -0500

    Fix a problematic migration

--- db/migrate/20140501161057_remove_assessment_resource_relationship.rb
+++ db/migrate/20140501161057_remove_assessment_resource_relationship.rb
@@ -1,7 +1,5 @@
 class RemoveAssessmentResourceRelationship < ActiveRecord::Migration
   def change
-    remove_reference :resources, :assessment
-    remove_reference :assessments, :resource
     remove_column :assessments, :resource_id if
         column_exists? :assessments, :resource_id
     remove_column :resources, :assessment_id if
