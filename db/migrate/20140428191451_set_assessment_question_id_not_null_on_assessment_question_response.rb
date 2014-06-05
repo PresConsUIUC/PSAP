e8f1d126fa093bea8cb6737b381dd5878472dea9
Alex Dolski <alexd@n-ary.net>
2014-06-04 21:49:49 -0500

    Fix a problematic migration

--- db/migrate/20140428191451_set_assessment_question_id_not_null_on_assessment_question_response.rb
+++ db/migrate/20140428191451_set_assessment_question_id_not_null_on_assessment_question_response.rb
@@ -1,6 +1,6 @@
 class SetAssessmentQuestionIdNotNullOnAssessmentQuestionResponse < ActiveRecord::Migration
   def change
-    change_column_null :assessment_question_responses, :assessment_question,
+    change_column_null :assessment_question_responses, :assessment_question_id,
                        false
   end
 end
