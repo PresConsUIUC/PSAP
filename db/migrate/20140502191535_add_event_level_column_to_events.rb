4dacd5a87f4ed6d2c8cd52e7d09907ba64a339c1
Alex Dolski <alexd@n-ary.net>
2014-06-04 21:58:00 -0500

    Fix a problematic migration

--- db/migrate/20140502191535_add_event_level_column_to_events.rb
+++ db/migrate/20140502191535_add_event_level_column_to_events.rb
@@ -1,6 +1,6 @@
 class AddEventLevelColumnToEvents < ActiveRecord::Migration
   def change
-    add_column :events, :event_level, :decimal, precision: 1, scale: 1,
+    add_column :events, :event_level, :decimal, precision: 1, scale: 0,
                default: 6 # EventLevel::INFO
     change_column_null :events, :event_level, false
   end
