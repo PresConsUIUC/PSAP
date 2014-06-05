e558c00fa97be7bb4cc0fe1209a6db8ddf20ea98
Alex Dolski <alexd@n-ary.net>
2014-06-04 21:51:50 -0500

    Fix a problematic migration

--- db/migrate/20140501155540_remove_assessment_question_option_ref_from_assessment_sections_again.rb
+++ db/migrate/20140501155540_remove_assessment_question_option_ref_from_assessment_sections_again.rb
@@ -1,6 +1,8 @@
 class RemoveAssessmentQuestionOptionRefFromAssessmentSectionsAgain < ActiveRecord::Migration
   def change
-    remove_column :assessment_sections, :assessment_question_option_id, :integer
-    remove_column :assessment_sections, :assessment_question_options_id, :integer
+    remove_column :assessment_sections, :assessment_question_option_id, :integer if
+        column_exists? :assessment_sections, :assessment_question_option_id
+    remove_column :assessment_sections, :assessment_question_options_id, :integer if
+        column_exists? :assessment_sections, :assessment_question_options_id
   end
 end
