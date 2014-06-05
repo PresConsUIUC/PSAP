2b8a07173731625ac0faed38cf6493be89fef7d2
Alex Dolski <alexd@n-ary.net>
2014-06-04 21:44:00 -0500

    Fix a problematic migration

--- db/migrate/20140310200911_add_resource_ref_to_assessment.rb
+++ db/migrate/20140310200911_add_resource_ref_to_assessment.rb
@@ -1,5 +1,9 @@
 class AddResourceRefToAssessment < ActiveRecord::Migration
   def change
+    create_table :assessments do |t|
+      t.string name
+      t.timestamps
+    end
     add_reference :assessments, :resource, index: true
   end
 end
