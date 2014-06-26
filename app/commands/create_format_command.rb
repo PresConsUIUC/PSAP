d129eab2589e100f019ca451d6312daae5cde230
Alex Dolski <alexd@n-ary.net>
2014-06-26 16:13:54 -0500

    Assign temperature ranges to new formats

--- app/commands/create_format_command.rb
+++ app/commands/create_format_command.rb
@@ -10,6 +10,17 @@ class CreateFormatCommand < Command
   def execute
     begin
       @format.save!
+
+      unless @format.temperature_ranges.any?
+        TemperatureRange.create!(min_temp_f: nil, max_temp_f: 32, score: 1,
+                                 format: @format)
+        TemperatureRange.create!(min_temp_f: 33, max_temp_f: 54, score: 0.67,
+                                 format: @format)
+        TemperatureRange.create!(min_temp_f: 55, max_temp_f: 72, score: 0.33,
+                                 format: @format)
+        TemperatureRange.create!(min_temp_f: 73, max_temp_f: nil, score: 0,
+                                 format: @format)
+      end
     rescue ActiveRecord::RecordInvalid
       @format.events << Event.create(
           description: "Attempted to create format, but failed: "\
