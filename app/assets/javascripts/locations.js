fac4036ad3b451278da6f14201c292dc58ba809a
Alex Dolski <alexd@n-ary.net>
2014-07-21 14:20:33 -0500

    Refactor checkbox code

--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -2,29 +2,43 @@ var ready = function() {
     if ($('body#show_location')) {
         var checkboxes = $('input[name="resources[]"]');
 
+        function refreshCheckboxUI() {
+            var all_checked = checkboxes.filter(':checked').length == checkboxes.length;
+            var none_checked = checkboxes.filter(':checked').length == 0;
+
+            $('button.psap-move-checked').prop('disabled', none_checked);
+            if (all_checked) {
+                $('.psap-check-all').prop('disabled', true);
+                $('.psap-uncheck-all').prop('disabled', false);
+            } else if (none_checked) {
+                $('.psap-check-all').prop('disabled', false);
+                $('.psap-uncheck-all').prop('disabled', true);
+            } else {
+                $('.psap-check-all').prop('disabled', false);
+                $('.psap-uncheck-all').prop('disabled', false);
+            }
+        }
+
         // hook up "check/uncheck all" buttons
-        $('#psap-check-all').on('click', function() {
+        $('.psap-check-all').on('click', function() {
             checkboxes.each(function() {
                 $(this).prop('checked', true);
             });
-            $('#psap-uncheck-all').show();
-            $(this).hide();
+            refreshCheckboxUI();
             checkboxes.trigger('change');
         });
-        $('#psap-uncheck-all').on('click', function() {
+        $('.psap-uncheck-all').on('click', function() {
             checkboxes.each(function() {
                 $(this).prop('checked', false);
             });
-            $('#psap-check-all').show();
-            $(this).hide();
+            refreshCheckboxUI();
             checkboxes.trigger('change');
         });
 
         // conditionally enable the "move checked" button depending on whether
         // any checkboxes are checked
         checkboxes.on('change', function() {
-            $('button#psap-move-checked').prop('disabled',
-                !checkboxes.is(':checked'));
+            refreshCheckboxUI();
         }).trigger('change');
     }
 };
