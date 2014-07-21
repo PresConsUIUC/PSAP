1753d3cd77c88a01fc788e7382a3d299b1411e18
Alex Dolski <alexd@n-ary.net>
2014-07-21 11:30:53 -0500

    Add UI for resource moving

---
+++ app/assets/javascripts/locations.js
@@ -0,0 +1,33 @@
+var ready = function() {
+    if ($('body#show_location')) {
+        var checkboxes = $('input[name="resources"]');
+
+        // hook up "check/uncheck all" buttons
+        $('#psap-check-all').on('click', function() {
+            checkboxes.each(function() {
+                $(this).prop('checked', true);
+            });
+            $('#psap-uncheck-all').show();
+            $(this).hide();
+            checkboxes.trigger('change');
+        });
+        $('#psap-uncheck-all').on('click', function() {
+            checkboxes.each(function() {
+                $(this).prop('checked', false);
+            });
+            $('#psap-check-all').show();
+            $(this).hide();
+            checkboxes.trigger('change');
+        });
+
+        // conditionally enable the "move checked" button depending on whether
+        // any checkboxes are checked
+        checkboxes.on('change', function() {
+            $('button#psap-move-checked').prop('disabled',
+                !checkboxes.is(':checked'));
+        }).trigger('change');
+    }
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
