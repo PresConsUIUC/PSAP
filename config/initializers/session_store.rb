901efeaa55627746cd288e3f886daa632bb22563
Alex Dolski <alexd@n-ary.net>
2014-07-22 08:59:24 -0500

    Remove one-hour session limit

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # 1-hour sessions are specified in the PSAP Software Requirements Specification
-Psap::Application.config.session_store :cookie_store, key: '_psap_session',
-                                       expire_after: 1.hour
+Psap::Application.config.session_store :cookie_store, key: '_psap_session'
