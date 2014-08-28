98241f66dbfbb71a6fc444514fb5887ec1efa8c3
Alex Dolski <alexd@n-ary.net>
2014-08-28 15:54:27 -0500

    Remove question counts

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -13,11 +13,4 @@ class Assessment < ActiveRecord::Base
     !new_record?
   end
 
-  def question_count
-    count = 0
-    self.assessment_sections.select{ |s|
-      count += s.assessment_questions.length }
-    count
-  end
-
 end
