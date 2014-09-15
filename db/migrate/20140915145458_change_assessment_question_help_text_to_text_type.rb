243b47ff9a3dd530f9a8a144ed80cac219f1122d
Alex Dolski <alexd@n-ary.net>
2014-09-15 10:00:38 -0500

    Remove limit

--- db/migrate/20140915145458_change_assessment_question_help_text_to_text_type.rb
+++ db/migrate/20140915145458_change_assessment_question_help_text_to_text_type.rb
@@ -1,5 +1,5 @@
 class ChangeAssessmentQuestionHelpTextToTextType < ActiveRecord::Migration
   def change
-    change_column :assessment_questions, :help_text, :text
+    change_column :assessment_questions, :help_text, :text, limit: nil
   end
 end
