67bf48ef95b9b833d958f7d4db4c3e981cb985b4
Alex Dolski <alexd@n-ary.net>
2014-09-15 11:32:26 -0500

    Fix a mysql-incompatible migration

--- db/migrate/20140425160626_change_type_of_assessment_question_option_value.rb
+++ db/migrate/20140425160626_change_type_of_assessment_question_option_value.rb
@@ -1,10 +1,5 @@
 class ChangeTypeOfAssessmentQuestionOptionValue < ActiveRecord::Migration
   def change
-    if AssessmentQuestionOption.columns_hash['value'].type == :string
-      change_column :assessment_question_options, :value,
-                    'float USING CAST(value AS float)'
-    else
-      change_column :assessment_question_options, :value, :float
-    end
+    change_column :assessment_question_options, :value, :float
   end
 end
