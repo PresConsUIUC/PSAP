8038c7fb7777f4e2cc72d44d3525bcea8a21486e
Alex Dolski <alexd@n-ary.net>
2014-11-25 15:48:54 -0600

    Decrease log level

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,8 @@ Psap::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.log_level = :info
+
   # Do not eager load code on boot.
   config.eager_load = false
 
