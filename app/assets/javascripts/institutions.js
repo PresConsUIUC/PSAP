fc1b709fa31846bef84a4a656c6bce8d3041431c
Alex Dolski <alexd@n-ary.net>
2015-01-09 10:23:47 -0600

    Remove institution form validation

--- app/assets/javascripts/institutions.js
+++ app/assets/javascripts/institutions.js
@@ -1,42 +1,7 @@
 var ready = function() {
-    if ($('body#new_institution')) {
-        EditInstitutionForm.attachEventListeners();
-    }
-
     if ($('body#show_institution')) {
         ShowInstitutionForm.attachEventListeners();
     }
-};
-
-var EditInstitutionForm = {
-
-    attachEventListeners: function() {
-        $('input#institution_name').bind('keyup input paste', function() {
-            PSAP.Form.validate('institution_name', 1, 255);
-        });
-        $('input#institution_url').bind('keyup input paste', function() {
-            PSAP.Form.validate('institution_url', 0, 0, Form.TYPE_URL);
-        });
-        $('input#institution_address1').bind('keyup input paste', function() {
-            PSAP.Form.validate('institution_address1', 1, 255);
-        });
-        $('input#institution_address2').bind('keyup input paste', function() {
-            PSAP.Form.validate('institution_address2', 1, 255);
-        });
-        $('input#institution_city').bind('keyup input paste', function() {
-            PSAP.Form.validate('institution_city', 1, 255);
-        });
-        $('input#institution_state').bind('keyup input paste', function() {
-            PSAP.Form.validate('institution_state', 1, 255);
-        });
-        $('input#institution_postal_code').bind('keyup input paste', function() {
-            PSAP.Form.validate('institution_postal_code', 1, 255);
-        });
-        $('#country').bind('change', function() {
-            PSAP.Form.validate('institution_country', 1, 255);
-        });
-    }
-
 };
 
 var ShowInstitutionForm = {
