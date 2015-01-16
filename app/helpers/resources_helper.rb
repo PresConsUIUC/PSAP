571fc56515ec4acfaa8a74db16798fcf63ce7d3b
Alex Dolski <alexd@n-ary.net>
2015-01-16 09:54:37 -0600

    Rename Parent Resource menu to Containing Collection, and show only collections

--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -1,5 +1,17 @@
 module ResourcesHelper
 
+  def collection_hierarchy_options_for_select(location)
+    collections = location.resources.
+        where(resource_type: ResourceType::COLLECTION, parent_id: nil).
+        order(:name)
+    options = []
+    collections.each do |collection|
+      level = 0
+      add_option_for_collection(collection, options, level)
+    end
+    options
+  end
+
   def human_readable_date(resource_date)
     if resource_date.year
       "#{resource_date.year} (#{resource_date.readable_date_type.downcase})"
@@ -7,16 +19,6 @@ module ResourcesHelper
       "#{resource_date.begin_year}-#{resource_date.end_year} "\
         "(#{resource_date.readable_date_type.downcase})"
     end
-  end
-
-  def resource_hierarchy_options_for_select(location)
-    resources = location.resources.where(parent_id: nil).order(:name)
-    options = []
-    resources.each do |resource|
-      level = 0
-      add_option_for_resource(resource, options, level)
-    end
-    options
   end
 
   def score_help(resource)
@@ -60,11 +62,12 @@ module ResourcesHelper
 
   private
 
-  def add_option_for_resource(resource, options, level)
+  def add_option_for_collection(collection, options, level)
     options << [raw(('&nbsp;&nbsp;&nbsp;&nbsp;' * level) +
-                        (level > 0 ? raw('&#8627; ') : '') + resource.name), resource.id]
-    resource.children.each do |child|
-      add_option_for_resource(child, options, level + 1)
+                        (level > 0 ? raw('&#8627; ') : '') + collection.name),
+                collection.id]
+    collection.children.where(resource_type: ResourceType::COLLECTION).each do |child|
+      add_option_for_collection(child, options, level + 1)
     end
   end
 
