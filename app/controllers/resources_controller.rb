c15501314b66b28ab169026bb6e95ac104460c61
Alex Dolski <alexd@n-ary.net>
2015-02-02 09:05:27 -0600

    Fix missing extensions for exported files

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -156,7 +156,7 @@ class ResourcesController < ApplicationController
     respond_to do |format|
       format.csv {
         response.headers['Content-Disposition'] =
-            "attachment; filename=\"#{@resource.filename}\""
+            "attachment; filename=\"#{@resource.filename}.csv\""
         render text: @resource.as_csv
       }
       format.html {
@@ -164,12 +164,12 @@ class ResourcesController < ApplicationController
       }
       format.dcxml {
         response.headers['Content-Disposition'] =
-            "attachment; filename=\"#{@resource.filename}\""
+            "attachment; filename=\"#{@resource.filename}.xml\""
         @institution = @resource.location.repository.institution
       }
       format.ead {
         response.headers['Content-Disposition'] =
-            "attachment; filename=\"#{@resource.filename}\""
+            "attachment; filename=\"#{@resource.filename}.xml\""
         @institution = @resource.location.repository.institution
       }
     end
