002347b73ed1945edd7f1010ea761828174fe88a
Alex Dolski <alexd@n-ary.net>
2015-05-08 16:04:16 -0500

    Add per-section PDF assessment reports (#282)

--- app/controllers/concerns/prawn_charting.rb
+++ app/controllers/concerns/prawn_charting.rb
@@ -10,7 +10,14 @@ module PrawnCharting
   extend ActiveSupport::Concern
   include AssessmentQuestionsHelper
 
+  FONT = 'Helvetica'
+  H1_SIZE = 18
+  H2_SIZE = 16
+  H3_SIZE = 14
+
   ##
+  # Returns the entire assessment report.
+  #
   # @param institution Institution
   # @param user User
   # @param institution_data 10-element array of numbers
@@ -24,57 +31,25 @@ module PrawnCharting
   def pdf_assessment_report(institution, user, institution_data,
                             collection_datas, location_assessment_sections,
                             institution_formats, collections)
-    pdf = Prawn::Document.new(
-        info: {
-            Title: "Assessment Report: #{institution.name}",
-            Author: user.full_name,
-            #Subject: "My Subject",
-            #Keywords: "test metadata ruby pdf dry",
-            Creator: "Preservation Self-Assessment Program (PSAP)",
-            Producer: "Prawn",
-            CreationDate: Time.now
-        })
-
-    font = 'Helvetica'
-    h1_size = 18
-    h2_size = 16
-    h3_size = 14
-    pdf.default_leading 2
-    page_width = pdf.bounds.right
+    pdf = pdf_skeleton(institution, user)
 
     # heading
-    heading(pdf, institution, font, h1_size)
+    heading(pdf, institution)
 
     # page 1: executive summary
-    pdf.font font, style: :bold
-    pdf.text 'Executive Summary', size: h1_size
-    pdf.font font, style: :normal
+    pdf.font FONT, style: :bold
+    pdf.text 'Executive Summary', size: H1_SIZE
+    pdf.font FONT, style: :normal
     pdf.move_down 20
 
     # institution
-    pdf.font font, style: :bold
-    pdf.text 'Institution', size: h2_size
-    pdf.font font, style: :normal
-
-    if institution.assessment_question_responses.any?
-      pdf.text "Score: #{(institution.assessment_score * 100).round}"
-
-      data = []
-      Assessment.find_by_key('institution').assessment_sections.each do |section|
-        score = institution.assessment_score_in_section(section)
-        data << [ section.name, (score * 100).round,
-                  assessment_score_characterization(score) ]
-      end
-      pdf.table(data, cell_style: { border_lines: [:dotted] })
-    else
-      pdf.text 'The institution has not been assessed yet.'
-    end
+    pdf_preservation_assessment_report(pdf, institution, user)
     pdf.move_down 20
 
     # locations
-    pdf.font font, style: :bold
-    pdf.text 'Storage Environments', size: h2_size
-    pdf.font font, style: :normal
+    pdf.font FONT, style: :bold
+    pdf.text 'Storage Environments', size: H2_SIZE
+    pdf.font FONT, style: :normal
 
     data = []
     Assessment.find_by_key('location').assessment_sections.each do |section|
@@ -92,9 +67,9 @@ module PrawnCharting
     pdf.move_down 20
 
     # resources
-    pdf.font font, style: :bold
-    pdf.text 'Resources', size: h2_size
-    pdf.font font, style: :normal
+    pdf.font FONT, style: :bold
+    pdf.text 'Resources', size: H2_SIZE
+    pdf.font FONT, style: :normal
 
     data = []
     Assessment.find_by_key('resource').assessment_sections.each do |section|
@@ -111,65 +86,30 @@ module PrawnCharting
 
     # page 2+: locations summary
     pdf.start_new_page
-    heading(pdf, institution, font, h1_size)
-    pdf.font font, style: :bold
-    pdf.text 'Storage Environments', size: h2_size
-    pdf.font font, style: :normal
-    pdf.move_down 20
-
-    if institution.locations.any?
-      data = []
-      row = ['Location']
-      location_assessment_sections.each do |section|
-        row << section.name
-      end
-      row << 'Overall'
-      data << row
-
-      institution.locations.order(:name).each do |location|
-        if location.assessment_question_responses.any?
-          row = [ location.name ]
-          Assessment.find_by_key('location').assessment_sections.each do |section|
-            row << (location.assessment_score_in_section(section) * 100).round
-          end
-          row << (location.assessment_score * 100).round
-          data << row
-        end
-      end
-
-      pdf.table(data, cell_style: { border_lines: [:dotted] })
-    else
-      pdf.text 'No locations have been assessed yet.'
-    end
+    pdf_storage_assessment_report(pdf, institution, user,
+                                  location_assessment_sections, false)
 
     # resources overview
     pdf.start_new_page
-    heading(pdf, institution, font, h1_size)
-    pdf.font font, style: :bold
-    pdf.text 'Resources', size: h2_size
-    pdf.font font, style: :normal
-    pdf.move_down 20
-
-    pdf.text "#{institution.resources.length} total items"
-    pdf.text "#{institution.all_assessed_items.length} items assessed"
-    if institution_formats.any?
-      pdf.text "Formats present:"
-      pdf.indent 10 do
-        institution_formats.each do |format|
-          pdf.text "• #{format.name}"
-        end
-      end
-    end
-    pdf.move_down 20
-
-    bar_chart(pdf, institution_data, page_width, 200)
+    pdf_resources_assessment_report(pdf, institution, user, institution_formats,
+                                    institution_data, false)
 
     # collections overview
     pdf.start_new_page
-    heading(pdf, institution, font, h1_size)
-    pdf.font font, style: :bold
-    pdf.text 'Collections', size: h2_size
-    pdf.font font, style: :normal
+    pdf_collections_assessment_report(pdf, institution, user, collections,
+                                      collection_datas, false)
+
+    add_page_numbers(pdf)
+    pdf
+  end
+
+  def pdf_collections_assessment_report(pdf, institution, user, collections,
+                                        collection_datas, page_numbers = true)
+    pdf = pdf_skeleton(institution, user) unless pdf
+    heading(pdf, institution)
+    pdf.font FONT, style: :bold
+    pdf.text 'Collections', size: H2_SIZE
+    pdf.font FONT, style: :normal
     pdf.move_down 20
 
     num_displayed_collections = 0
@@ -179,16 +119,16 @@ module PrawnCharting
         if all_assessed_items.any?
           num_displayed_collections += 1
 
-          pdf.font font, style: :bold
-          pdf.text collection.name, size: h3_size
-          pdf.font font, style: :normal
+          pdf.font FONT, style: :bold
+          pdf.text collection.name, size: H3_SIZE
+          pdf.font FONT, style: :normal
           pdf.move_down 10
 
           # left column
           y_pos = pdf.cursor
-          col_height = pdf.bounds.top / 2 - pdf.height_of('bla', size: h2_size) -
-              pdf.height_of('bla', size: h3_size) * 2
-          pdf.bounding_box([0, y_pos], width: page_width / 2, height: col_height) do
+          col_height = pdf.bounds.top / 2 - pdf.height_of('bla', size: H2_SIZE) -
+              pdf.height_of('bla', size: H3_SIZE) * 2
+          pdf.bounding_box([0, y_pos], width: pdf.bounds.right / 2, height: col_height) do
             pdf.text "Assessment: #{AssessmentType::name_for_type(collection.assessment_type)}"
             pdf.text "#{all_assessed_items.length} items assessed"
             pdf.text "Assessed by:"
@@ -199,8 +139,8 @@ module PrawnCharting
               end
             end
             pdf.text "Last assessed: #{
-            all_assessed_items.sort{ |x,y| x.updated_at <=> y.updated_at }.
-                last.updated_at.strftime('%B %d, %Y')}"
+                     all_assessed_items.sort{ |x,y| x.updated_at <=> y.updated_at }.
+                         last.updated_at.strftime('%B %d, %Y')}"
             pdf.text "Formats present:"
             all_assessed_items.collect{ |r| r.format }.uniq{ |f| f.id }.each do |format|
               pdf.indent 10 do
@@ -228,8 +168,8 @@ module PrawnCharting
           end
 
           # right column
-          pdf.bounding_box([page_width / 2, y_pos],
-                           width: page_width / 2, height: col_height) do
+          pdf.bounding_box([pdf.bounds.right / 2, y_pos],
+                           width: pdf.bounds.right / 2, height: col_height) do
             bar_chart(pdf, collection_datas[collection.id],
                       pdf.bounds.right, col_height / 1.2)
             #pdf.stroke_bounds
@@ -243,16 +183,108 @@ module PrawnCharting
     else
       pdf.text 'This institution has no collections.'
     end
+    add_page_numbers(pdf) if page_numbers
+    pdf
+  end
 
-    pdf.number_pages '<page>', { start_count_at: 2,
-                                 page_filter: lambda{ |pg| pg != 1 },
-                                 at: [page_width - 50, pdf.bounds.top],
-                                 align: :right,
-                                 size: 10 }
+  def pdf_preservation_assessment_report(pdf, institution, user)
+    unless pdf
+      pdf = pdf_skeleton(institution, user)
+      heading(pdf, institution)
+    end
+    pdf.font FONT, style: :bold
+    pdf.text 'Institution', size: H2_SIZE
+    pdf.font FONT, style: :normal
+
+    if institution.assessment_question_responses.any?
+      pdf.text "Score: #{(institution.assessment_score * 100).round}"
+      data = []
+      Assessment.find_by_key('institution').assessment_sections.each do |section|
+        score = institution.assessment_score_in_section(section)
+        data << [ section.name, (score * 100).round,
+                  assessment_score_characterization(score) ]
+      end
+      pdf.table(data, cell_style: { border_lines: [:dotted] })
+    else
+      pdf.text 'The institution has not been assessed yet.'
+    end
+    pdf
+  end
+
+  def pdf_resources_assessment_report(pdf, institution, user,
+                                      institution_formats, institution_data,
+                                      page_numbers = true)
+    pdf = pdf_skeleton(institution, user) unless pdf
+    heading(pdf, institution)
+    pdf.font FONT, style: :bold
+    pdf.text 'Resources', size: H2_SIZE
+    pdf.font FONT, style: :normal
+    pdf.move_down 20
+
+    pdf.text "#{institution.resources.length} total items"
+    pdf.text "#{institution.all_assessed_items.length} items assessed"
+    if institution_formats.any?
+      pdf.text "Formats present:"
+      pdf.indent 10 do
+        institution_formats.each do |format|
+          pdf.text "• #{format.name}"
+        end
+      end
+    end
+    pdf.move_down 20
+    bar_chart(pdf, institution_data, pdf.bounds.right, 200)
+
+    add_page_numbers(pdf) if page_numbers
+    pdf
+  end
+
+  def pdf_storage_assessment_report(pdf, institution, user,
+                                    location_assessment_sections,
+                                    page_numbers = true)
+    pdf = pdf_skeleton(institution, user) unless pdf
+    heading(pdf, institution)
+    pdf.font FONT, style: :bold
+    pdf.text 'Storage Environments', size: H2_SIZE
+    pdf.font FONT, style: :normal
+    pdf.move_down 20
+
+    if institution.locations.any?
+      data = []
+      row = ['Location']
+      location_assessment_sections.each do |section|
+        row << section.name
+      end
+      row << 'Overall'
+      data << row
+
+      institution.locations.order(:name).each do |location|
+        if location.assessment_question_responses.any?
+          row = [ location.name ]
+          Assessment.find_by_key('location').assessment_sections.each do |section|
+            row << (location.assessment_score_in_section(section) * 100).round
+          end
+          row << (location.assessment_score * 100).round
+          data << row
+        end
+      end
+
+      pdf.table(data, cell_style: { border_lines: [:dotted] })
+    else
+      pdf.text 'No locations have been assessed yet.'
+    end
+    add_page_numbers(pdf) if page_numbers
     pdf
   end
 
   private
+
+  def add_page_numbers(pdf)
+    pdf.number_pages '<page>', { start_count_at: 2,
+                                 page_filter: lambda{ |pg| pg != 1 },
+                                 at: [pdf.bounds.right - 50, pdf.bounds.top],
+                                 align: :right,
+                                 size: 10 }
+  end
 
   ##
   # @param pdf Prawn::Document
@@ -329,22 +361,35 @@ module PrawnCharting
   ##
   # @param pdf Prawn::Document
   # @param institution Institution
-  # @param font string
-  # @param h1_size integer
   # @return void
   #
-  def heading(pdf, institution, font, h1_size)
+  def heading(pdf, institution)
     pdf.image File.join(Rails.root, 'app', 'assets', 'images', 'psap_logo@2x.png'),
               at: [0, pdf.bounds.top], width: 100
     pdf.indent 110 do
-      pdf.font font, style: :bold
-      pdf.text 'Assessment Report', size: h1_size
-      pdf.font font, style: :normal
+      pdf.font FONT, style: :bold
+      pdf.text 'Assessment Report', size: H1_SIZE
+      pdf.font FONT, style: :normal
       pdf.text institution.name
       pdf.text Time.now.strftime('%B %-d, %Y'), size: 10
     end
     pdf.stroke_horizontal_rule
     pdf.move_down 20
+  end
+
+  def pdf_skeleton(institution, user)
+    pdf = Prawn::Document.new(
+        info: {
+            Title: "Assessment Report: #{institution.name}",
+            Author: user.full_name,
+            #Subject: "My Subject",
+            Keywords: 'preservation assessment',
+            Creator: 'Preservation Self-Assessment Program (PSAP)',
+            Producer: 'Prawn',
+            CreationDate: Time.now
+        })
+    pdf.default_leading 2
+    pdf
   end
 
 end
