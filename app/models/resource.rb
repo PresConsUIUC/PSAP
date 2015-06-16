PresConsUIUC/PSAP

commit 3c160f84fcc447f9f476539cfe6f61f556b393f5

Move ResourceType to Resource::Type

app/models/resource.rb
@@ -1,6 +1,6 @@
 ##
 # A Resource exists within a Location. It has a resource_type property which
-# must be set to either ResourceType::ITEM or ResourceType::COLLECTION.
+# must be set to either Resource::Type::ITEM or Resource::Type::COLLECTION.
 # Collections can contain zero or more child resources. Formats, ink/media
 # types, and support types can be ascribed only to items.
 #
@@ -8,6 +8,15 @@
 # score is the mean of the item scores contained within.
 #
 class Resource < ActiveRecord::Base
+
+  class Type
+    COLLECTION = 0
+    ITEM = 1
+
+    def self.all
+      return (0..1)
+    end
+  end
 
   # When adding/removing properties or associations, update both .as_csv and
   # ::as_csv.
@@ -217,9 +226,9 @@ class Resource < ActiveRecord::Base
     doc.xpath('//ead:archdesc', ead_ns).each do |element|
       case element.attribute('level').text.strip
         when 'collection'
-          params[:resource_type] = ResourceType::COLLECTION
+          params[:resource_type] = Resource::Type::COLLECTION
         else
-          params[:resource_type] = ResourceType::ITEM
+          params[:resource_type] = Resource::Type::ITEM
       end
     end
 
@@ -310,7 +319,7 @@ class Resource < ActiveRecord::Base
   # in the hierarchy.
   #
   def all_assessed_items
-    all_children.select{ |x| x.resource_type == ResourceType::ITEM and
+    all_children.select{ |x| x.resource_type == Resource::Type::ITEM and
         x.assessment_complete }
   end
 
@@ -472,7 +481,7 @@ class Resource < ActiveRecord::Base
   # @return float between 0 and 1
   #
   def effective_assessment_score
-    if self.resource_type == ResourceType::COLLECTION
+    if self.resource_type == Resource::Type::COLLECTION
       items = self.all_assessed_items
       return items.any? ?
           items.map(&:assessment_score).reduce(:+) / items.length.to_f : 0.0
@@ -560,7 +569,7 @@ class Resource < ActiveRecord::Base
   end
 
   def prune_irrelevant_models
-    if self.resource_type == ResourceType::COLLECTION
+    if self.resource_type == Resource::Type::COLLECTION
       self.format = nil
       self.format_ink_media_type = nil
       self.format_support_type = nil
@@ -576,9 +585,9 @@ class Resource < ActiveRecord::Base
 
   def readable_resource_type
     case resource_type
-      when ResourceType::COLLECTION
+      when Resource::Type::COLLECTION
         'Collection'
-      when ResourceType::ITEM
+      when Resource::Type::ITEM
         'Item'
     end
   end
@@ -627,13 +636,13 @@ class Resource < ActiveRecord::Base
   end
 
   def validates_item_children
-    if self.resource_type == ResourceType::ITEM and self.children.any?
+    if self.resource_type == Resource::Type::ITEM and self.children.any?
       errors[:base] << 'Non-empty collections cannot be changed into items.'
     end
   end
 
   def validates_not_child_of_item
-    if parent and parent.resource_type != ResourceType::COLLECTION
+    if parent and parent.resource_type != Resource::Type::COLLECTION
       errors[:base] << 'Only collection resources can have sub-resources.'
     end
   end

test/models/resource_test.rb
@@ -58,7 +58,7 @@ class ResourceTest < ActiveSupport::TestCase
   test 'setting a resource to a collection should prune its AQRs' do
     response = assessment_question_responses(:assessment_question_response_one)
     @resource.assessment_question_responses << response
-    @resource.resource_type = ResourceType::COLLECTION
+    @resource.resource_type = Resource::Type::COLLECTION
     @resource.save
     assert !@resource.assessment_question_responses.any?
   end
@@ -137,9 +137,9 @@ class ResourceTest < ActiveSupport::TestCase
 
   # readable_resource_type
   test 'readable_resource_type should work' do
-    @resource.resource_type = ResourceType::COLLECTION
+    @resource.resource_type = Resource::Type::COLLECTION
     assert_equal 'Collection', @resource.readable_resource_type
-    @resource.resource_type = ResourceType::ITEM
+    @resource.resource_type = Resource::Type::ITEM
     assert_equal 'Item', @resource.readable_resource_type
   end
 

app/controllers/assessment_report_controller.rb
@@ -19,11 +19,11 @@ class AssessmentReportController < ApplicationController
     end
     if [nil, 'collections'].include?(params[:section])
       @collections = @institution.resources.
-          where(resource_type: ResourceType::COLLECTION)
+          where(resource_type: Resource::Type::COLLECTION)
       @collection_chart_datas = {}
       @collections.each do |collection|
         sub_collections = collection.all_children.select{ |r|
-          r.resource_type == ResourceType::COLLECTION }
+          r.resource_type == Resource::Type::COLLECTION }
         sub_collections << collection
         parent_ids = sub_collections.map{ |r| r.id }.join(', ')
         data = []

app/controllers/concerns/prawn_charting.rb
@@ -27,7 +27,7 @@ module PrawnCharting
   # @param location_assessment_sections Array of AssessmentSections
   # @param institution_formats
   # @param collections Array of Resources of
-  # resource_type == ResourceType::COLLECTION
+  # resource_type == Resource::Type::COLLECTION
   #
   def pdf_assessment_report(institution, user, institution_data,
                             collection_datas, location_assessment_sections,

app/helpers/application_helper.rb
@@ -176,7 +176,7 @@ module ApplicationHelper
     elsif entity.kind_of?(Repository) or entity == Repository
       class_ = 'fa-building-o'
     elsif entity.kind_of?(Resource)
-      if entity.resource_type == ResourceType::COLLECTION
+      if entity.resource_type == Resource::Type::COLLECTION
         class_ = 'fa-cubes'
       elsif entity.format
         case entity.format.format_class

app/helpers/resources_helper.rb
@@ -2,7 +2,7 @@ module ResourcesHelper
 
   def collection_hierarchy_options_for_select(location)
     collections = location.resources.
-        where(resource_type: ResourceType::COLLECTION, parent_id: nil).
+        where(resource_type: Resource::Type::COLLECTION, parent_id: nil).
         order(:name)
     options = []
     collections.each do |collection|
@@ -102,7 +102,7 @@ module ResourcesHelper
     options << [raw(('&nbsp;&nbsp;&nbsp;&nbsp;' * level) +
                         (level > 0 ? raw('&#8627; ') : '') + collection.name),
                 collection.id]
-    collection.children.where(resource_type: ResourceType::COLLECTION).each do |child|
+    collection.children.where(resource_type: Resource::Type::COLLECTION).each do |child|
       add_option_for_collection(child, options, level + 1)
     end
   end

app/models/institution.rb
@@ -140,7 +140,7 @@ class Institution < ActiveRecord::Base
   end
 
   def mean_resource_score
-    items = resources.where(resource_type: ResourceType::ITEM,
+    items = resources.where(resource_type: Resource::Type::ITEM,
                             assessment_complete: true)
     items.any? ?
         items.map(&:assessment_score).reduce(:+).to_f / items.length.to_f : 0.0

app/models/resource_type.rb
@@ -1,10 +1,0 @@
-class ResourceType
-
-  COLLECTION = 0;
-  ITEM = 1;
-
-  def self.all
-    return (0..1)
-  end
-
-end

commit 37dccb0b88bb3353756991178af8df08b8c99471

Fix bugs introduced in 3c160f8

app/models/resource.rb
@@ -70,7 +70,7 @@ class Resource < ActiveRecord::Base
                          message: 'must be a valid assessment type' }
   validates :location, presence: true
   validates :name, presence: true, length: { maximum: 255 }
-  validates :resource_type, inclusion: { in: ResourceType.all,
+  validates :resource_type, inclusion: { in: Resource::Type.all,
                          message: 'must be a valid resource type' }
   validates :significance, allow_blank: true,
             inclusion: { in: Resource::Significance.all,

test/models/resource_test.rb
@@ -160,7 +160,7 @@ class ResourceTest < ActiveSupport::TestCase
   end
 
   test 'resource_type is in bounds' do
-    @resource.resource_type = ResourceType.all.last + 1
+    @resource.resource_type = Resource::Type.all.last + 1
     assert !@resource.save
   end
 