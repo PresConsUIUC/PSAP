04797cb708c5701ab35a28f2867e6e7d70f24f33
Alex Dolski <alexd@n-ary.net>
2015-06-25 10:53:10 -0500

    Add back a dashboard redirect -- unknown why this was missing...

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,7 @@
 class StaticController < ApplicationController
 
+  before_action :signed_in_user, only: :landing
+
   def about
   end
 
@@ -13,6 +15,11 @@ class StaticController < ApplicationController
   end
 
   def landing
+  end
+
+  # Overrides parent
+  def signed_in_user
+    redirect_to dashboard_url if signed_in?
   end
 
 end
