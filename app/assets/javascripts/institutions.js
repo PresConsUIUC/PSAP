972247b9a7f5717e4c89efa992c91225c8067f4d
Alex Dolski <alexd@n-ary.net>
2015-06-25 09:49:26 -0500

    Fix create-repository panel

--- app/assets/javascripts/institutions.js
+++ app/assets/javascripts/institutions.js
@@ -1,16 +1,14 @@
 var ready = function() {
     if ($('body#show_institution').length) {
-        if ($('input[name="subpage"]').val() == 'repositories') {
-            // initialize create-repository panel
-            PSAP.Panel.initRemote(
-                '#psap-create-panel',
-                $('input[name="new_repository_url"]').val(),
-                function () {
-                    PSAP.Form.init();
-                }
-            );
-        }
-
+        // initialize create-repository panel
+        PSAP.Panel.initRemote(
+            '#psap-create-panel',
+            $('input[name="new_repository_url"]').val(),
+            function () {
+                PSAP.Form.init();
+            }
+        );
+        
         // initialize edit-institution panel
         PSAP.Panel.initRemote(
             '#psap-edit-panel',
