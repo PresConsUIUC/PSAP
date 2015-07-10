8977ccf229b1271dd5e2d660c8ddda48c00f1fd5
Alex Dolski <alexd@n-ary.net>
2015-07-10 11:26:53 -0500

    Change *_filters to *_actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :init
-  after_filter :flash_in_response_headers
+  before_action :init
+  after_action :flash_in_response_headers
 
   layout 'application'
 
