1734e2f6caf5fac7ea3f0f8d68c46e536bb2af17
Alex Dolski <alexd@n-ary.net>
2016-01-19 14:03:50 -0600

    Move Test Institution to the top of the institution select menu (#336)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -96,6 +96,13 @@ class UsersController < ApplicationController
   def edit
     @roles = Role.all.order(:name)
     @user_role = Role.find_by_name('User')
+
+    # Gather institutions for the institution select menu. Put the test
+    # institution at the top per GitHub issue #336.
+    test_institution_id = Psap::Application.psap_config[:test_institution_id]
+    @institutions = Institution.where('id != ?', test_institution_id).
+        order(:name)
+    @institutions.unshift(Institution.find(test_institution_id))
   end
 
   ##
