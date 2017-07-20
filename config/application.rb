9125c4ab2a0ad28d9a0f4aaa227cef83b91a015d
Alex Dolski <alexd@n-ary.net>
2017-07-20 12:41:20 -0500

    Disable ActiveRecord logging

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module Psap
 
     attr_accessor :psap_config
 
+    config.after_initialize do
+      ActiveRecord::Base.logger = nil
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
