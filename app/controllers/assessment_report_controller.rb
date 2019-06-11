PresConsUIUC/PSAP

commit 5b885eb8e74887d57f5398163fc3f616652674c1

Replace some usages of map() with pluck()

app/controllers/assessment_report_controller.rb
@@ -41,8 +41,10 @@ class AssessmentReportController < ApplicationController
       end
     end
     if [nil, 'resources'].include?(params[:section])
-      @institution_formats = @institution.resources.collect{ |r| r.format }.
-          select{ |f| f }.uniq{ |f| f.id }
+      @institution_formats = @institution.resources
+                                 .pluck(:format_id)
+                                 .select{ |f| f }
+                                 .uniq
       @resource_chart_data = []
       (0..9).each do |i|
         sql = "SELECT COUNT(resources.id) AS count "\

app/models/resource.rb
@@ -327,14 +327,14 @@ class Resource < ActiveRecord::Base
             [resource.format_ink_media_type ? resource.format_ink_media_type.name : nil] +
             [resource.format_support_type ? resource.format_support_type.name : nil] +
             [resource.readable_significance] +
-            resource.creators.map(&:name) + [nil] * (num_columns[:creator] - resource.creators.length) +
+            resource.creators.pluck(:name) + [nil] * (num_columns[:creator] - resource.creators.length) +
             resource.resource_dates.map(&:as_dublin_core_string) + [nil] * (num_columns[:date] - resource.resource_dates.length) +
             [language_name] +
-            resource.subjects.map(&:name) + [nil] * (num_columns[:subject] - resource.subjects.length) +
-            resource.extents.map(&:name) + [nil] * (num_columns[:extent] - resource.extents.length) +
+            resource.subjects.pluck(:name) + [nil] * (num_columns[:subject] - resource.subjects.length) +
+            resource.extents.pluck(:name) + [nil] * (num_columns[:extent] - resource.extents.length) +
             [resource.rights] +
             [resource.description] +
-            resource.resource_notes.map(&:value) + [nil] * (num_columns[:note] - resource.resource_notes.length) +
+            resource.resource_notes.pluck(:value) + [nil] * (num_columns[:note] - resource.resource_notes.length) +
             [resource.created_at.iso8601] +
             [resource.updated_at.iso8601]
       end
@@ -490,14 +490,14 @@ class Resource < ActiveRecord::Base
           [self.format_ink_media_type ? self.format_ink_media_type.name : nil] +
           [self.format_support_type ? self.format_support_type.name : nil] +
           [self.readable_significance] +
-          self.creators.map { |r| r.name } +
+          self.creators.pluck(:name) +
           self.resource_dates.map { |r| r.as_dublin_core_string } +
           [self.language ? self.language.english_name : nil] +
-          self.subjects.map { |s| s.name } +
-          self.extents.map { |e| e.name } +
+          self.subjects.pluck(:name) +
+          self.extents.pluck(:name) +
           [self.rights] +
           [self.description] +
-          self.resource_notes.map { |n| n.value } +
+          self.resource_notes.pluck(:value) +
           [self.created_at.iso8601] +
           [self.updated_at.iso8601] +
           responses

commit 5ef0a6f8f05b6e7b5eed363fd5e6cabec3cbf266

Fix a bug introduced in 5b885eb8

app/controllers/assessment_report_controller.rb
@@ -57,8 +57,8 @@ class AssessmentReportController < ApplicationController
     # Resources section
     if [nil, 'resources'].include?(params[:section])
       @institution_formats = @institution.resources
-                                 .pluck(:format_id)
-                                 .select{ |f| f }
+                                 .map(&:format)
+                                 .select(&:present?)
                                  .uniq
       @resource_chart_data = []
       10.times do |i|