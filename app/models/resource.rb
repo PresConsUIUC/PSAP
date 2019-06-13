PresConsUIUC/PSAP

commit e7a1c1a7541f437858d1037bdb2f7ab5e51b83d0

Replace Resource.effective_assessment_score() with assessment_score() and move the previous assessment_score() implementation to isolated_assessment_score()

app/models/resource.rb
@@ -85,7 +85,6 @@ class Resource < ApplicationRecord
 
   validates_uniqueness_of :name, scope: :parent_id
 
-  after_initialize :init
   before_save :update_assessment_score, :update_assessment_complete
 
   ##
@@ -274,11 +273,11 @@ class Resource < ApplicationRecord
     # find the max number of one-to-many columns needed
     num_columns = { creator: 0, date: 0, subject: 0, extent: 0, note: 0 }
     resources.each do |resource|
-      num_columns[:creator] = [resource.creators.length, num_columns[:creator]].max
-      num_columns[:date] = [resource.resource_dates.length, num_columns[:date]].max
-      num_columns[:subject] = [resource.subjects.length, num_columns[:subject]].max
-      num_columns[:extent] = [resource.extents.length, num_columns[:extent]].max
-      num_columns[:note] = [resource.resource_notes.length, num_columns[:note]].max
+      num_columns[:creator] = [resource.creators.count, num_columns[:creator]].max
+      num_columns[:date]    = [resource.resource_dates.count, num_columns[:date]].max
+      num_columns[:subject] = [resource.subjects.count, num_columns[:subject]].max
+      num_columns[:extent]  = [resource.extents.count, num_columns[:extent]].max
+      num_columns[:note]    = [resource.resource_notes.count, num_columns[:note]].max
     end
 
     require 'csv'
@@ -318,7 +317,7 @@ class Resource < ApplicationRecord
         end
         csv << [resource.local_identifier] +
             [resource.name] +
-            [(resource.effective_assessment_score * 100).round(2)] +
+            [(resource.assessment_score * 100).round(2)] +
             [Assessment::Type::name_for_type(resource.assessment_type)] +
             [resource.location.name] +
             [resource.readable_resource_type] +
@@ -523,7 +522,7 @@ class Resource < ApplicationRecord
           questions
       csv << [self.local_identifier] +
           [self.name] +
-          [(self.effective_assessment_score * 100).round(2)] +
+          [(self.assessment_score * 100).round(2)] +
           [Assessment::Type::name_for_type(self.assessment_type)] +
           [self.location.name] +
           [self.readable_resource_type] +
@@ -533,7 +532,7 @@ class Resource < ApplicationRecord
           [self.format_support_type ? self.format_support_type.name : nil] +
           [self.readable_significance] +
           self.creators.pluck(:name) +
-          self.resource_dates.map { |r| r.as_dublin_core_string } +
+          self.resource_dates.map(&:as_dublin_core_string) +
           [self.language ? self.language.english_name : nil] +
           self.subjects.pluck(:name) +
           self.extents.pluck(:name) +
@@ -553,20 +552,22 @@ class Resource < ApplicationRecord
   # @return [Hash] with :mean, :median, :low, and :high keys
   #
   def assessed_item_statistics
-    stats = { mean: 0, median: 0, low: nil, high: 0 }
-    all_items = all_assessed_items
-    if all_items.length < 1
+    stats           = { mean: 0, median: 0, low: nil, high: 0 }
+    all_items       = all_assessed_items
+    all_items_count = all_items.count
+    if all_items_count < 1
       return nil
     end
 
     all_items.each do |item|
-      stats[:high] = item.effective_assessment_score if item.effective_assessment_score > stats[:high]
-      stats[:low] = item.effective_assessment_score if
-          stats[:low].nil? or item.effective_assessment_score < stats[:low]
+      stats[:high] = item.assessment_score if item.assessment_score > stats[:high]
+      stats[:low]  = item.assessment_score if
+          stats[:low].nil? or item.assessment_score < stats[:low]
     end
-    stats[:mean] = all_items.map{ |r| r.effective_assessment_score }.sum.to_f / all_items.length.to_f
-    sorted = all_items.map{ |r| r.effective_assessment_score }.sort
-    len = sorted.length
+    all_scores     = all_items.pluck(:assessment_score)
+    stats[:mean]   = all_scores.sum.to_f / all_items_count.to_f
+    sorted         = all_scores.sort
+    len            = sorted.length
     stats[:median] = (sorted[(len - 1) / 2] + sorted[len / 2]) / 2.0
     stats
   end
@@ -628,22 +629,6 @@ class Resource < ApplicationRecord
   end
 
   ##
-  # Returns the score of all assessment question responses, excluding location,
-  # temperature, etc.
-  #
-  # @return [float] between 0 and 1.
-  #
-  def assessment_score
-    question_score = 0.0
-    assessment_question_responses.map(&:assessment_question).
-        select{ |r| r.parent_id.nil? }.uniq.each do |top_q|
-      question_score += assessment_question_score(top_q)
-    end
-    # scores are pre-weighted; max is 50 so have to multiply by 2
-    (question_score / 100) * 2
-  end
-
-  ##
   # @return [ActiveRecord::Relation<Resource>]
   #
   def collections
@@ -686,32 +671,6 @@ class Resource < ApplicationRecord
     end
 
     clone
-  end
-
-  ##
-  # Returns the canonical assessment score of the resource, factoring in the
-  # location, temperature, and RH scores as well. If a collection, returns the
-  # average score of all resources.
-  #
-  # See https://github.com/PresConsUIUC/PSAP/wiki/Scoring
-  #
-  # @return [Float] Float between 0 and 1. The result is cached.
-  #
-  def effective_assessment_score
-    if @effective_assessment_score < 0
-      if self.resource_type == Resource::Type::COLLECTION
-        items = self.all_assessed_items
-        @effective_assessment_score = items.count > 0 ?
-            items.map(&:assessment_score).reduce(:+) / items.length.to_f : 0.0
-      else
-        @effective_assessment_score = self.assessment_score * 0.5 +
-            self.effective_format_score * 0.4 +
-            self.location.assessment_score * 0.05 +
-            self.effective_temperature_score * 0.025 +
-            self.effective_humidity_score * 0.025
-      end
-    end
-    @effective_assessment_score
   end
 
   ##
@@ -794,14 +753,31 @@ class Resource < ApplicationRecord
   def full_export_as_json
     struct = self.as_json
     struct[:assessment_question_responses] =
-        self.assessment_question_responses.map { |r| r.as_json }
-    struct[:creators]       = self.creators.map { |r| r.as_json }
-    struct[:extents]        = self.extents.map { |r| r.as_json }
-    struct[:resource_dates] = self.resource_dates.map { |r| r.as_json }
-    struct[:resource_notes] = self.resource_notes.map { |r| r.as_json }
-    struct[:subjects]       = self.subjects.map { |r| r.as_json }
-    struct[:resources]      = self.children.map { |r| r.full_export_as_json }
+        self.assessment_question_responses.map(&:as_json)
+    struct[:creators]       = self.creators.map(&:as_json)
+    struct[:extents]        = self.extents.map(&:as_json)
+    struct[:resource_dates] = self.resource_dates.map(&:as_json)
+    struct[:resource_notes] = self.resource_notes.map(&:as_json)
+    struct[:subjects]       = self.subjects.map(&:as_json)
+    struct[:resources]      = self.children.map(&:full_export_as_json)
     struct
+  end
+
+  ##
+  # @return [Float] Assessment score of the instance itself, excluding location,
+  #                 format, etc.
+  #
+  def isolated_assessment_score
+    question_score = 0.0
+    assessment_question_responses
+        .map(&:assessment_question)
+        .select{ |r| r.parent_id.nil? }
+        .uniq
+        .each do |top_q|
+      question_score += assessment_question_score(top_q)
+    end
+    # scores are pre-weighted; max is 50 so have to multiply by 2
+    (question_score / 100) * 2
   end
 
   ##
@@ -852,7 +828,7 @@ class Resource < ApplicationRecord
   end
 
   def sync_location_with_parent
-    self.location = self.parent.location if self.parent
+    self.location = self.parent&.location
   end
 
   ##
@@ -864,11 +840,24 @@ class Resource < ApplicationRecord
   end
 
   ##
-  # Overrides Assessable mixin
+  # Overrides the Assessable mixin to assign the effective assessment score
+  # (taking into account location, format, etc.) to `assessment_score`. For
+  # collections, assigns the average score of all child resources.
+  #
+  # See https://github.com/PresConsUIUC/PSAP/wiki/Scoring
   #
   def update_assessment_score
-    self.assessment_score = self.effective_assessment_score
-    @effective_assessment_score = -1
+    if self.resource_type == Resource::Type::COLLECTION
+      scores = self.all_assessed_items.pluck(:assessment_score).select(&:present?)
+      self.assessment_score = scores.length > 0 ?
+                                  scores.sum / scores.length.to_f : 0.0
+    else
+      self.assessment_score = isolated_assessment_score * 0.5 +
+          self.effective_format_score * 0.4 +
+          self.location.assessment_score * 0.05 +
+          self.effective_temperature_score * 0.025 +
+          self.effective_humidity_score * 0.025
+    end
   end
 
   private
@@ -882,12 +871,8 @@ class Resource < ApplicationRecord
     parts
   end
 
-  def init
-    @effective_assessment_score = -1
-  end
-
   def validate_item_children
-    if self.resource_type == Resource::Type::ITEM and self.children.any?
+    if self.resource_type == Resource::Type::ITEM and self.children.count > 0
       errors[:base] << 'Non-empty collections cannot be changed into items.'
     end
   end

app/helpers/resources_helper.rb
@@ -54,7 +54,7 @@ module ResourcesHelper
   end
 
   def score_help(resource)
-    assessment_score = (resource.assessment_score * 100).round(1)
+    assessment_score = (resource.isolated_assessment_score * 100).round(1)
     format_score = (resource.effective_format_score * 100).round(1)
     location_score = (resource.location.assessment_score * 100).round(1)
     temp_score = (resource.effective_temperature_score * 100).round(1)

app/models/institution.rb
@@ -77,23 +77,25 @@ class Institution < ApplicationRecord
   # Returns a hash containing statistics of all assessed items in the
   # institution.
   #
-  # @return hash with mean, median, low, and high keys
+  # @return [Hash] Hash with mean, median, low, and high keys
   #
   def assessed_item_statistics
     stats = { mean: 0, median: 0, low: 0, high: 0 }
     all_items = all_assessed_items
-    if all_items.length < 1
+    all_items_count = all_items.count
+    if all_items_count < 1
       return stats
     end
 
     all_items.each do |item|
-      stats[:high] = item.effective_assessment_score if item.effective_assessment_score > stats[:high]
-      stats[:low] = item.effective_assessment_score if
-          stats[:low].nil? or item.effective_assessment_score < stats[:low]
+      stats[:high] = item.assessment_score if item.assessment_score > stats[:high]
+      stats[:low]  = item.assessment_score if
+          stats[:low].nil? or item.assessment_score < stats[:low]
     end
-    stats[:mean] = all_items.map{ |r| r.effective_assessment_score }.sum.to_f / all_items.length.to_f
-    sorted = all_items.map{ |r| r.effective_assessment_score }.sort
-    len = sorted.length
+    all_scores     = all_items.pluck(:assessment_score)
+    stats[:mean]   = all_scores.sum.to_f / all_items_count.to_f
+    sorted         = all_scores.sort
+    len            = sorted.length
     stats[:median] = (sorted[(len - 1) / 2] + sorted[len / 2]) / 2.0
     stats
   end
@@ -121,8 +123,8 @@ class Institution < ApplicationRecord
   def full_export_as_json
     struct = self.as_json
     struct[:assessment_question_responses] =
-        self.assessment_question_responses.map { |r| r.as_json }
-    struct[:repositories] = self.repositories.map { |repo| repo.full_export_as_json }
+        self.assessment_question_responses.map(&:as_json)
+    struct[:repositories] = self.repositories.map(&:full_export_as_json)
     struct
   end
 
@@ -158,16 +160,15 @@ class Institution < ApplicationRecord
 
   def mean_location_score
     locations_ = locations.where(assessment_complete: true)
-    locations_.any? ?
-        locations_.map(&:assessment_score).reduce(:+).to_f / locations_.length.to_f :
-        0.0
+    count = locations_.count
+    count > 0 ? locations_.pluck(:assessment_score).sum.to_f / count.to_f : 0.0
   end
 
   def mean_resource_score
     items = resources.where(resource_type: Resource::Type::ITEM,
                             assessment_complete: true)
-    items.any? ?
-        items.map(&:assessment_score).reduce(:+).to_f / items.length.to_f : 0.0
+    count = items.count
+    count > 0 ? items.pluck(:assessment_score).sum.to_f / count.to_f : 0.0
   end
 
   def to_s

app/views/resources/_show.html.erb
@@ -119,7 +119,7 @@ breadcrumbs << @resource.name
 
   <div class="col-sm-4 psap-entity-nav-menu">
     <%= render partial: 'shared/entity_hierarchy',
-               locals: { selected_entity: @resource }%>
+               locals: { selected_entity: @resource.location }%>
   </div>
 
   <div class="col-sm-8">
@@ -134,7 +134,7 @@ breadcrumbs << @resource.name
       </li>
       <li>
         <a href="#score_summary_tab" data-toggle="tab">Score Summary
-          <span class="badge"><%= number_to_human(@resource.effective_assessment_score * 100) %></span>
+          <span class="badge"><%= number_to_human(@resource.assessment_score * 100) %></span>
         </a>
       </li>
       <li><a href="#activity_tab" data-toggle="tab">Activity</a></li>
@@ -336,8 +336,8 @@ breadcrumbs << @resource.name
             <% # TODO: use the assessable_report partial %>
             <div class="psap-assessable-report">
               <div class="psap-progress-container">
-                <span class="psap-score <%= bootstrap_fg_class_for_score(@resource.effective_assessment_score) %>">
-                  <%= number_to_human(@resource.effective_assessment_score * 100) %>
+                <span class="psap-score <%= bootstrap_fg_class_for_score(@resource.assessment_score) %>">
+                  <%= number_to_human(@resource.assessment_score * 100) %>
                   <% if pct_complete < 1 %>
                     *
                   <% end %>
@@ -346,11 +346,11 @@ breadcrumbs << @resource.name
                         data-toggle="popover" class="btn btn-default help"
                         data-content="<%= score_help(@resource) %>">?</button>
                 <div class="progress">
-                  <div class="progress-bar <%= bootstrap_progress_bar_class_for_score(@resource.effective_assessment_score) %> score"
+                  <div class="progress-bar <%= bootstrap_progress_bar_class_for_score(@resource.assessment_score) %> score"
                        role="progressbar"
-                       aria-valuenow="<%= @resource.effective_assessment_score * 100 %>"
+                       aria-valuenow="<%= @resource.assessment_score * 100 %>"
                        aria-valuemin="0" aria-valuemax="100"
-                       style="width:<%= @resource.effective_assessment_score * 100 %>%">
+                       style="width:<%= @resource.assessment_score * 100 %>%">
                   </div>
                 </div>
               </div>
@@ -381,15 +381,15 @@ breadcrumbs << @resource.name
           <% end %>
         <% else %>
           <div class="psap-progress-container">
-            <span class="psap-score <%= bootstrap_fg_class_for_score(@resource.effective_assessment_score) %>">
-              <%= number_to_human(@resource.effective_assessment_score * 100) %>
+            <span class="psap-score <%= bootstrap_fg_class_for_score(@resource.assessment_score) %>">
+              <%= number_to_human(@resource.assessment_score * 100) %>
             </span>
             <div class="progress">
-              <div class="progress-bar <%= bootstrap_progress_bar_class_for_score(@resource.effective_assessment_score) %> score"
+              <div class="progress-bar <%= bootstrap_progress_bar_class_for_score(@resource.assessment_score) %> score"
                    role="progressbar"
-                   aria-valuenow="<%= @resource.effective_assessment_score * 100 %>"
+                   aria-valuenow="<%= @resource.assessment_score * 100 %>"
                    aria-valuemin="0" aria-valuemax="100"
-                   style="width:<%= @resource.effective_assessment_score * 100 %>%">
+                   style="width:<%= @resource.assessment_score * 100 %>%">
               </div>
             </div>
           </div>

app/views/resources/search.json.jbuilder
@@ -2,7 +2,7 @@ json.resources @resources do |resource|
   json.psap_id resource.id
   json.local_identifier resource.local_identifier
   json.name resource.name
-  json.assessment_score_percent resource.effective_assessment_score.to_f
+  json.assessment_score_percent resource.assessment_score.to_f
   json.assessment_type Assessment::Type::name_for_type(resource.assessment_type)
   json.location resource.location.name
   json.resource_type resource.readable_resource_type

test/models/resource_test.rb
@@ -210,17 +210,13 @@ class ResourceTest < ActiveSupport::TestCase
 
   # as_csv
   test 'as_csv should work' do
-    skip # TODO: write this
+    csv = @resource.as_csv
+    assert csv.end_with?("\n")
   end
 
   # assessed_item_statistics
   test 'assessed_item_statistics should work' do
     skip # TODO: write this
-  end
-
-  # assessment_score
-  test 'assessment_score should calculate properly' do
-    assert_equal 0.01, @resource.assessment_score.to_f
   end
 
   # dup
@@ -247,11 +243,6 @@ class ResourceTest < ActiveSupport::TestCase
     assert_equal "Clone of Clone of #{'a' * (Resource::MAX_NAME_LENGTH - 18)}",
                  clone2.name
     assert clone2.name.length <= Resource::MAX_NAME_LENGTH
-  end
-
-  # effective_assessment_score
-  test 'effective_assessment_score should work' do
-    skip # TODO: write this
   end
 
   # effective_format_score
@@ -313,6 +304,12 @@ class ResourceTest < ActiveSupport::TestCase
   test 'update_assessment_complete should work when a format with assessment questions is set and a response exists' do
     @resource.update_assessment_complete
     assert @resource.assessment_complete
+  end
+
+  # update_assessment_score
+  test 'update_assessment_score should calculate properly' do
+    @resource.update_assessment_score
+    assert_equal 0.005, @resource.assessment_score.to_f
   end
 
   ########################### association tests ##############################

commit 817af41ced2d2674e2fb332d3a2b822b1b5be89f

Fix a bug introduced in e7a1c1a7

app/models/resource.rb
@@ -827,7 +827,7 @@ class Resource < ApplicationRecord
   end
 
   def sync_location_with_parent
-    self.location = self.parent&.location
+    self.location = self.parent.location if self.parent
   end
 
   ##